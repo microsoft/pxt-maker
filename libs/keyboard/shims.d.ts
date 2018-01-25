25d75e7f9175b55c45461d794ec035542b6d7bee
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-25 13:51:18 -0800

    barebone support for HID Keyboard (#9)
    * Adding keyboard support
    
    * updated common packages version

---
+++ libs/keyboard/shims.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+declare namespace keyboard {
+
+    /**
+     * Sends a sequence of keystrokes to the keyboard
+     */
+    //% blockId=keyboardType block="keyboard type %text" shim=keyboard::type
+    function type(text: string): void;
+}
+
+// Auto-generated. Do not edit. Really.
