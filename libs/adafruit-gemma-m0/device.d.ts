c8d5ae5bbdb52ebf90a75e10bf01b354006b669c
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-05 17:26:48 -0800

    more gemma work

--- libs/adafruit-gemma-m0/device.d.ts
+++ libs/adafruit-gemma-m0/device.d.ts
@@ -1,46 +1,45 @@
 declare namespace pins {
     //% fixedInstance shim=pxt::getPin(PIN_D0)
-    const D0: PwmPin;
+    const D0: DigitalPin;
     //% fixedInstance shim=pxt::getPin(PIN_D1)
-    const D1: PwmPin;
+    const D1: DigitalPin;
     //% fixedInstance shim=pxt::getPin(PIN_D2)
-    const D2: PwmPin;
+    const D2: DigitalPin;
 
-    //% fixedInstance shim=pxt::getPin(PIN_D13)
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
     const LED: DigitalPin;
 
-    //% fixedInstance shim=pxt::getPin(PIN_D0)
+    //% fixedInstance shim=pxt::getPin(PIN_SDA)
     const SDA: DigitalPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D2)
+    //% fixedInstance shim=pxt::getPin(PIN_SCL)
     const SCL: DigitalPin;
 
-    //% fixedInstance shim=pxt::getPin(PIN_D3)
+    //% fixedInstance shim=pxt::getPin(PIN_SCK)
     const SCK: DigitalPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D2)
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
     const MISO: DigitalPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D4)
+    //% fixedInstance shim=pxt::getPin(PIN_OSI)
     const MOSI: DigitalPin;
 }
 
-declare namespace input {
+declare namespace input {    
+    /**
+     * Capacitive pin D0
+     */
+    //% block="pin D0" fixedInstance shim=pxt::getTouchButton(PIN_D0)
+    const pinD0: TouchButton;
+
     /**
      * Capacitive pin D1
      */
     //% block="pin D1" fixedInstance shim=pxt::getTouchButton(PIN_D1)
     const pinD1: TouchButton;
 
-
     /**
      * Capacitive pin D4
      */
-    //% block="pin D4" fixedInstance shim=pxt::getTouchButton(PIN_D4)
+    //% block="pin D2" fixedInstance shim=pxt::getTouchButton(PIN_D2)
     const pinD2: TouchButton;
-    
-    /**
-     * Capacitive pin D3
-     */
-    //% block="pin D3" fixedInstance shim=pxt::getTouchButton(PIN_D3)
-    const pinD3: TouchButton;
 
     /**
      * Button connecting D0 to GND.
@@ -49,7 +48,6 @@ declare namespace input {
     //% shim=pxt::getButtonByPin(PIN_D0,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonD0: Button;
 
-
     /**
      * Button connecting D1 to GND.
      */
@@ -57,18 +55,10 @@ declare namespace input {
     //% shim=pxt::getButtonByPin(PIN_D1,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonD1: Button;
 
-
     /**
      * Button connecting D2 to GND.
      */
     //% block="button D2" fixedInstance
     //% shim=pxt::getButtonByPin(PIN_D2,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonD2: Button;
-        
-    /**
-     * Button connecting D3 to GND.
-     */
-    //% block="button D3" fixedInstance
-    //% shim=pxt::getButtonByPin(PIN_D3,BUTTON_ACTIVE_LOW_PULL_UP)
-    const buttonD3: Button;
 }
