faf37a64e71de10f78db0f3cebfbfd845b5189ca
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-05 17:17:52 -0800

    gemma

---
+++ libs/adafruit-gemma-m0/config.ts
@@ -0,0 +1,16 @@
+
+namespace config {
+    export const PIN_D0 = DAL.PA08;
+    export const PIN_D1 = DAL.PA02;
+    export const PIN_D2 = DAL.PA09;
+
+    export const PIN_A1 = DAL.PA09;
+
+    export const PIN_LED = DAL.PA17;
+    export const PIN_SDA = DAL.PA08;
+    export const PIN_SCL = DAL.PA09;
+
+    export const PIN_SCK = DAL.PA07;
+    export const PIN_MISO = 0;
+    export const MOSI = 0;    
+}
