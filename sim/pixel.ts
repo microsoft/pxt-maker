1104dc988a5517518c855d867f36b62bc0ce3d0d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-12 16:19:31 -0700

    Pixel to common (#39)
    * moving pixel to common
    
    * using pixelboard
    
    * updated pxt refs

--- sim/pixel.ts
+++
@@ -1,6 +0,0 @@
-namespace pxsim.pixel {
-    export const defaultPin = light.defaultPin;
-    export function sendBuffer(data: RefBuffer) {
-        light.sendBuffer((board() as DalBoard).neopixelPin as any, 1, data)
-    }
-}
