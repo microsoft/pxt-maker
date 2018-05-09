e6749ec8b0318edbeb5ac2694e72db4fddf402aa
Michał Moskal <michal@moskal.me>
2018-05-09 11:08:19 -0700

    New experimental boards and fixes around SPI (#50)
    * Allow for palette inversion
    
    * Add sample screens configs
    
    * Bump codal
    
    * First draft of F130RE support
    
    * Make it build
    
    * Regenerate dal.d.ts
    
    * Bump codal
    
    * Screen fixes
    
    * Fix button pin assignments
    
    * SPI fixes etc
    
    * Board config
    
    * Pin assignment for the hardware
    
    * Close to F103 end-to-end
    
    * Fix SPI in BLE Nano

--- libs/pxt-32-basic/device.d.ts
+++ libs/pxt-32-basic/device.d.ts
@@ -1,36 +1,22 @@
 declare namespace pins {
-    //% fixedInstance shim=pxt::getPin(PIN_A0)
-    const A0: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A1)
-    const A1: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A2)
-    const A2: PwmPin;
-
     //% fixedInstance shim=pxt::getPin(PIN_D0)
     const D0: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_D1)
     const D1: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_D2)
-    const D2: PwmOnlyPin;
+    const D2: AnalogInPin;
     //% fixedInstance shim=pxt::getPin(PIN_D3)
-    const D3: PwmOnlyPin;
+    const D3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D4)
+    const D4: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D5)
+    const D5: DigitalPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D6)
+    const D6: PwmOnlyPin;
     
-    //% fixedInstance shim=pxt::getPin(P0_9)
-    const MENU: PwmOnlyPin;
     
-    /*
-    //% fixedInstance shim=pxt::getPin(P0_13)
-    const LED1: DigitalPin;
-
-    //% fixedInstance shim=pxt::getPin(P0_14)
-    const LED2: DigitalPin;
-
-    //% fixedInstance shim=pxt::getPin(P0_15)
-    const LED3: DigitalPin;
-
-    //% fixedInstance shim=pxt::getPin(P0_16)
-    const LED4: DigitalPin;
-    */
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
+    const LED: DigitalPin;
 
     //% fixedInstance shim=pxt::getPin(PIN_SDA)
     const SDA: DigitalPin;
@@ -51,24 +37,24 @@ declare namespace pins {
 }
 
 declare namespace input {
-    //% block="button 1" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_11,BUTTON_ACTIVE_LOW_PULL_UP)
+        //% block="button 1" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D5,BUTTON_ACTIVE_LOW_PULL_UP)
     const button1: Button;
     //% block="button 2" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_13,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% shim=pxt::getButtonByPin(PIN_D4,BUTTON_ACTIVE_LOW_PULL_UP)
     const button2: Button;
     //% block="button 3" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_30,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% shim=pxt::getButtonByPin(PIN_D1,BUTTON_ACTIVE_LOW_PULL_UP)
     const button3: Button;
     //% block="button 4" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_28,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% shim=pxt::getButtonByPin(PIN_D3,BUTTON_ACTIVE_LOW_PULL_UP)
     const button4: Button;
 
     //% block="button E0" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_29,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% shim=pxt::getButtonByPin(PIN_D2,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonE0: Button;
     //% block="button E1" fixedInstance
-    //% shim=pxt::getButtonByPin(P0_10,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% shim=pxt::getButtonByPin(PIN_D0,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonE1: Button;
 }
 
