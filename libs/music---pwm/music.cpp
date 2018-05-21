fdf13b937d5f965d898783ea5ddfd54ba3343171
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-21 08:28:28 -0700

    fix default value in music pwm

--- libs/music---pwm/music.cpp
+++ libs/music---pwm/music.cpp
@@ -7,7 +7,7 @@ namespace music {
 /**
 * Play a tone through the speaker for some amount of time.
 * @param frequency pitch of the tone to play in Hertz (Hz), eg: Note.C
-* @param ms tone duration in milliseconds (ms), eg: music.beat(BeatFraction.Half)
+* @param ms tone duration in milliseconds (ms), eg: BeatFraction.Half
 */
 //% help=music/play-tone
 //% blockId=music_play_note block="play tone|at %note=device_note|for %duration=device_beat"
