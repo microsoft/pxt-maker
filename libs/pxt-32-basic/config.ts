93818b75f3712c138ba9d6645a1e7a36c8522086
Michal Moskal <mimoskal@microsoft.com>
2018-05-31 18:04:37 -0700

    Pinmap fixes

--- libs/pxt-32-basic/config.ts
+++ libs/pxt-32-basic/config.ts
@@ -1,29 +1,30 @@
 
 namespace config {
-    export const PIN_LED = DAL.PA_5;
+    export const PIN_LED = DAL.PC_5;
     export const PIN_SCK = DAL.PA_5;
     export const PIN_MISO = DAL.PA_6;
     export const PIN_MOSI = DAL.PA_7;
     export const PIN_RX = DAL.PA_10;
     export const PIN_TX = DAL.PA_9;
 
-    export const PIN_D0 = DAL.PA_10;
-    export const PIN_D1 = DAL.PA_9;
-    export const PIN_D2 = DAL.PA_0;
-    export const PIN_D3 = DAL.PA_1;
+    export const PIN_D0 = DAL.PA_3;
+    export const PIN_D1 = DAL.PA_2;
+    export const PIN_D2 = DAL.PA_10;
+    export const PIN_D3 = DAL.PB_3;
     export const PIN_D4 = DAL.PB_5;
-    export const PIN_D5 = DAL.PB_6;
-    export const PIN_D6 = DAL.PA_8;
-    export const PIN_D7 = DAL.PA_11;
-    export const PIN_D8 = DAL.PC_11;
-    export const PIN_D9 = DAL.PC_10;
-    export const PIN_D10 = DAL.PA_4;
+    export const PIN_D5 = DAL.PB_4;
+    export const PIN_D6 = DAL.PB_10;
+    export const PIN_D7 = DAL.PA_8;
+    export const PIN_D8 = DAL.PA_9;
+    export const PIN_D9 = DAL.PC_7;
+    export const PIN_D10 = DAL.PB_6;
+
     export const PIN_D11 = DAL.PA_7;
     export const PIN_D12 = DAL.PA_6;
     export const PIN_D13 = DAL.PA_5;
-    export const PIN_D14 = DAL.PB_8;
 
-    export const PIN_D15 = DAL.PC_0;
+    export const PIN_D14 = DAL.PB_9;
+    export const PIN_D15 = DAL.PB_8;
 
 
     export const PIN_DISPLAY_CS = PIN_D9;
@@ -31,7 +32,8 @@ namespace config {
     export const PIN_DISPLAY_MOSI = PIN_D11;
     export const PIN_DISPLAY_DC = PIN_D10;
     export const PIN_DISPLAY_RST = PIN_D14;
-    export const PIN_DISPLAY_MISO = PIN_D12; // NC
+    export const PIN_DISPLAY_MISO = -1; // NC
+
 
     /*
     export const PIN_DISPLAY_MOSI = DAL.PB_15; // 34
@@ -42,7 +44,7 @@ namespace config {
     */
 
     // it's really piezo speaker, not an amp
-    export const PIN_SPEAKER_AMP = PIN_D15; // SDA/RX3
+    export const PIN_SPEAKER_AMP = PIN_D12;
 
     // 128x128
     //export const DISPLAY_CFG0 = 0x00010248;
@@ -53,12 +55,12 @@ namespace config {
     //export const DISPLAY_CFG1 = 0x000603;
 
     // ST7735 from ALI
-    //export const DISPLAY_CFG0 = 0x01000090;
-    //export const DISPLAY_CFG1 = 0x000603;
+    export const DISPLAY_CFG0 = 0x01000090;
+    export const DISPLAY_CFG1 = 0x000603;
 
     // ILI9163 from ALI
-    export const DISPLAY_CFG0 = 0x00000090;
-    export const DISPLAY_CFG1 = 0x000e14ff;
+    //export const DISPLAY_CFG0 = 0x00000090;
+    //export const DISPLAY_CFG1 = 0x000e14ff;
 
     export const DISPLAY_CFG2 = 18; // MHz
     export const DISPLAY_WIDTH = 160;
