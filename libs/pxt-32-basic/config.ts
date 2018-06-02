19039983bf706fbaebaefc36c1f42a3c899de4a9
Michal Moskal <michal@moskal.me>
2018-06-02 08:12:46 -0700

    Settings for BP 1.1

--- libs/pxt-32-basic/config.ts
+++ libs/pxt-32-basic/config.ts
@@ -1,7 +1,7 @@
 
 namespace config {
     export const PIN_LED = DAL.PC_8;
-    
+
     export const PIN_SCK = DAL.PA_5;
     export const PIN_MISO = DAL.PA_6;
     export const PIN_MOSI = DAL.PA_7;
@@ -15,24 +15,25 @@ namespace config {
     export const PIN_D14 = DAL.PB_9;
     export const PIN_D15 = DAL.PB_8;
 
-    export const PIN_BTN_LEFT = DAL.PA_15;
-    export const PIN_BTN_UP = DAL.PA_5;
-    export const PIN_BTN_RIGHT = DAL.PC_13;
-    export const PIN_BTN_DOWN = DAL.PB_10;
-    
+    export const PIN_BTN_LEFT = DAL.PB_10;
+    export const PIN_BTN_UP = DAL.PA_15;
+    export const PIN_BTN_RIGHT = DAL.PA_5;
+    export const PIN_BTN_DOWN = DAL.PC_13;
+
     export const PIN_BTN_A = PIN_SDA;
     export const PIN_BTN_B = PIN_SCL;
 
 
-    export const PIN_DISPLAY_CS = DAL.PC_3;
-    export const PIN_DISPLAY_SCK = DAL.PB_3;
-    export const PIN_DISPLAY_MOSI = DAL.PB_5;
-    export const PIN_DISPLAY_DC = DAL.PA_7;
-    export const PIN_DISPLAY_RST = DAL.PA_6;
-    export const PIN_DISPLAY_MISO = DAL.PB_4;
+    export const PIN_DISPLAY_CS = DAL.PB_12;
+    export const PIN_DISPLAY_SCK = DAL.PB_13;
+    export const PIN_DISPLAY_MOSI = DAL.PB_15;
+    export const PIN_DISPLAY_DC = DAL.PC_5;
+    export const PIN_DISPLAY_RST = DAL.PC_4;
+    export const PIN_DISPLAY_MISO = DAL.PB_14;
+    export const PIN_DISPLAY_BL = DAL.PA_4;
 
     // it's really piezo speaker, not an amp
-    export const PIN_SPEAKER_AMP = DAL.PA_8;
+    export const PIN_SPEAKER_AMP = DAL.PB_8;
 
     // 128x128
     //export const DISPLAY_CFG0 = 0x00010248;
@@ -42,9 +43,13 @@ namespace config {
     //export const DISPLAY_CFG0 = 0x00000040;
     //export const DISPLAY_CFG1 = 0x000603;
 
-    // ST7735 from ALI
-    export const DISPLAY_CFG0 = 0x01000090;
+    // BP 1.1
+    export const DISPLAY_CFG0 = 0x00000080;
     export const DISPLAY_CFG1 = 0x000603;
+
+    // ST7735 from ALI
+    //export const DISPLAY_CFG0 = 0x01000090;
+    //export const DISPLAY_CFG1 = 0x000603;
 
     // ILI9163 from ALI
     //export const DISPLAY_CFG0 = 0x00000090;
