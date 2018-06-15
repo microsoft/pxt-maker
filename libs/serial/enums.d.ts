f20d7ec77ff96fe65bfa8b3927291e027fb3b183
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-15 08:54:39 -0700

    UART support (#58)
    * serial blocks
    
    * updated shims
    
    * updating codal
    
    * updated codal for cplay
    
    * bump pxt
    
    * updated pxt reference

---
+++ libs/serial/enums.d.ts
@@ -0,0 +1,31 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum BaudRate {
+    //% block=115200
+    BaudRate115200 = 115200,
+    //% block=57600
+    BaudRate57600 = 57600,
+    //% block=38400
+    BaudRate38400 = 38400,
+    //% block=31250
+    BaudRate31250 = 31250,
+    //% block=28800
+    BaudRate28800 = 28800,
+    //% block=19200
+    BaudRate19200 = 19200,
+    //% block=14400
+    BaudRate14400 = 14400,
+    //% block=9600
+    BaudRate9600 = 9600,
+    //% block=4800
+    BaudRate4800 = 4800,
+    //% block=2400
+    BaudRate2400 = 2400,
+    //% block=1200
+    BaudRate1200 = 1200,
+    //% block=300
+    BaudRate300 = 300,
+    }
+
+// Auto-generated. Do not edit. Really.
