9768c03066358af7101a05d8708055c44aed6e0f
sabas1080 <s@theinventorhouse.org>
2018-08-05 19:29:51 -0500

    Update pins define

--- libs/arduino-mkr1300/config.ts
+++ libs/arduino-mkr1300/config.ts
@@ -17,22 +17,25 @@ namespace config {
     export const PIN_D3 = DAL.PA11; // Pwm
     export const PIN_D4 = DAL.PB10;
     export const PIN_D5 = DAL.PB11;
-    export const PIN_D6 = DAL.PA20;
-    export const PIN_D7 = DAL.PA14; // SS
+    export const PIN_D6 = DAL.PA20; //LED
+    export const PIN_D7 = DAL.PA21;
     export const PIN_D8 = DAL.PA16;
     export const PIN_D9 = DAL.PA17;
     export const PIN_D10 = DAL.PA19;
     export const PIN_D11 = DAL.PA08; // SDA, Pwm
     export const PIN_D12 = DAL.PA09; // SCL, Pwm
-    export const PIN_D13 = DAL.PB09; //BOOT LORA
-    export const PIN_D14 = DAL.PA27; //RESET LORA
+    export const PIN_D13 = DAL.PB23;
+    export const PIN_D14 = DAL.PA22;
     export const PIN_LED = DAL.PA20;
+
+    export const PIN_D15 = DAL.PB09; //BOOT LORA
+    export const PIN_A7 = DAL.PA27; //RESET LORA
 
     //Module LORA
     export const PIN_MISO = DAL.PA15; // MOSI
     export const PIN_SCK = DAL.PA13; // SCK
     export const PIN_MOSI = DAL.PA12; // MISO
-    // SS = PA14
+    export const PIN_FLASH_CS = DAL.PA14; // SS = PA14
 
     export const PIN_SDA = DAL.PA08; // SDA
     export const PIN_SCL = DAL.PA09; // SCL
