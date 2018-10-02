ba1da0f156f0cb590039c32a027b64e85d25cf3d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-02 15:59:33 -0700

    initial pin map

---
+++ libs/sparkfun-redbear/README.md
@@ -0,0 +1,1 @@
+# SparkFun Redbear
---
+++ libs/sparkfun-redbear/config.ts
@@ -0,0 +1,39 @@
+namespace config {
+    export const PIN_A0 = DAL.PA02;
+    export const PIN_A1 = DAL.PB08;
+    export const PIN_A2 = DAL.PB09;
+    export const PIN_A3 = DAL.PA04;
+    export const PIN_A4 = DAL.PA05;
+    export const PIN_A5 = DAL.PB02;
+
+    export const PIN_D0 = DAL.PA11;
+    export const PIN_D1 = DAL.PA10;
+    export const PIN_D2 = DAL.PA14;
+    export const PIN_D3 = DAL.PA09;
+    export const PIN_D4 = DAL.PA08;
+    export const PIN_D5 = DAL.PA15;
+    export const PIN_D6 = DAL.PA20;
+    export const PIN_D7 = DAL.PA21;
+    export const PIN_D8 = DAL.PA06;
+    export const PIN_D9 = DAL.PA07;
+    export const PIN_D10 = DAL.PA18;
+    export const PIN_D11 = DAL.PA16;
+    export const PIN_D12 = DAL.PA19;
+    export const PIN_D13 = DAL.PA17;
+
+    export const PIN_LED = DAL.PA30; // check
+    export const PIN_RX = DAL.PA11; // check
+    export const PIN_TX = DAL.PA10; // check
+    export const PIN_MISO = DAL.PA12;
+    export const PIN_MOSI = DAL.PB10;
+    export const PIN_SCK = DAL.PB11;
+    export const PIN_SCL = DAL.PA23;
+    export const PIN_SDA = DAL.PA22;
+    export const PIN_RXLED = DAL.PA31;
+    export const PIN_TXLED = DAL.PA27;
+
+    export const PIN_FLASH_MISO = DAL.PB03;
+    export const PIN_FLASH_MOSI = DAL.PB22;
+    export const PIN_FLASH_SCK = DAL.PB23;
+    export const PIN_FLASH_CS = DAL.PA13;
+}
