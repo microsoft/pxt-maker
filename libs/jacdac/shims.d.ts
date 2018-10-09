101b7305fff3462b2def94b431dda1a08f95c0b1
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-08 19:38:01 -0700

    adding jacdac

---
+++ libs/jacdac/shims.d.ts
@@ -0,0 +1,22 @@
+// Auto-generated. Do not edit.
+
+
+declare interface JacDacDriverStatus {
+    /** Check if driver is connected. */
+    //% property shim=JacDacDriverStatusMethods::isConnected
+    isConnected: boolean;
+
+    /** Get device class. */
+    //% property shim=JacDacDriverStatusMethods::driverClass
+    driverClass: uint32;
+
+    /** Get device class. */
+    //% property shim=JacDacDriverStatusMethods::driverAddress
+    driverAddress: uint8;
+
+    /** Get device id for events. */
+    //% property shim=JacDacDriverStatusMethods::deviceId
+    deviceId: boolean;
+}
+
+// Auto-generated. Do not edit. Really.
