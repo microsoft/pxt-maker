7d4e773112f86900b1cc83591b3b2bd8f3652b10
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-17 22:26:28 -0700

    dmx project

---
+++ libs/dmx/enums.d.ts
@@ -0,0 +1,5 @@
+// Auto-generated. Do not edit.
+declare namespace dmx {
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ libs/dmx/shims.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Functions to operate colored LEDs.
+     */
+    //% weight=100 color="#0078d7" icon="\uf00a"
+declare namespace dmx {
+}
+
+// Auto-generated. Do not edit. Really.
