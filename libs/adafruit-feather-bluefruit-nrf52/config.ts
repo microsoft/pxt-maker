462a2261e73c0551344145dee3770e5049cca39d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-06 14:07:40 -0800

    initial changes

---
+++ libs/adafruit-feather-bluefruit-nrf52/config.ts
@@ -0,0 +1,17 @@
+namespace config {
+    export const PIN_A0 = DAL.P0_2;
+    export const PIN_A1 = DAL.P0_3;
+    export const PIN_A2 = DAL.P0_4;
+    export const PIN_A3 = DAL.P0_5;
+    export const PIN_A4 = DAL.P0_28;
+    export const PIN_A5 = DAL.P0_29;
+
+    export const PIN_LED = DAL.P0_17;
+    export const PIN_RX = DAL.P0_8;
+    export const PIN_TX = DAL.P0_6;
+    export const PIN_MISO = DAL.P0_14;
+    export const PIN_MOSI = DAL.P0_13;
+    export const PIN_SCK = DAL.P0_12;
+    export const PIN_SCL = DAL.P0_26;
+    export const PIN_SDA = DAL.P0_25;
+}
