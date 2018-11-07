c9798ad48610e4f0036fe78a99221c7b4d39482a
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-07 10:25:45 -0800

    updated board support

--- libs/adafruit-itsybitsy-m0-express/config.ts
+++ libs/adafruit-itsybitsy-m0-express/config.ts
@@ -11,8 +11,6 @@ namespace config {
     export const PIN_D1 = DAL.PA10;
     export const PIN_D0 = DAL.PA11;
 
-    export const PIN_MISO = DAL.PA12;
-
     export const PIN_D2 = DAL.PA14;
     export const PIN_D5 = DAL.PA15;
     export const PIN_D11 = DAL.PA10;
@@ -22,19 +20,22 @@ namespace config {
     export const PIN_D6 = DAL.PA20;
     export const PIN_D7 = DAL.PA21;
 
+
+    export const PIN_A5 = DAL.PB02;
+    export const PIN_A1 = DAL.PB08;
+    export const PIN_A2 = DAL.PB09;
+
+    export const PIN_MISO = DAL.PA12;
+    export const PIN_MOSI = DAL.PB10;
+    export const PIN_SCK = DAL.PB11;
+
     export const PIN_SDA = DAL.PA22;
     export const PIN_SCL = DAL.PA23;
 
     export const PIN_FLASH_CS = DAL.PA27;
-
-    export const PIN_A5 = DAL.PB02;
     export const PIN_FLASH_MISO = DAL.PB03;
-    export const PIN_A1 = DAL.PB08;
-    export const PIN_A2 = DAL.PB09;
-
-    export const PIN_MOSI = DAL.PB10;
-    export const PIN_SCK = DAL.PB11;
-
     export const PIN_FLASH_MOSI = DAL.PB22;
     export const PIN_FLASH_SCK = DAL.PB23;
+
+    export const PIN_LED = DAL.PA13;
 }
