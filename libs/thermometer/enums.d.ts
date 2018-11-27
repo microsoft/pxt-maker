9f24ebc88b5b7a03742efc4fa6d0218bd4080dd4
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-26 16:41:45 -0800

    No mbed for SAMD21 (#97)
    * Compile cpx with mbed-less codal-samd
    
    * Use light not pixel on cpx
    
    * updated tag
    
    * updated tag
    
    * Use codal build system 0.6.1
    
    * handle new JACDAC ctor
    
    * upgrading linked codal
    
    * link updated common packages
    
    * undo some commit changes
    
    * fixing commit
    
    * fix branches location
    
    * bump pxt
    
    * updated shims
    
    * bump common packages
    
    * adding a few projects
    
    * updated board.json
    
    * missing accelerometer project
    
    * adding trinket

---
+++ libs/thermometer/enums.d.ts
@@ -0,0 +1,19 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum TemperatureCondition {
+    //% block="hot"
+    Hot = 2,  // ANALOG_THRESHOLD_HIGH
+    //% block="cold"
+    Cold = 1,  // ANALOG_THRESHOLD_LOW
+    }
+
+
+    declare const enum TemperatureUnit {
+    //% block="°C"
+    Celsius = 0,
+    //% block="°F"
+    Fahrenheit = 1,
+    }
+
+// Auto-generated. Do not edit. Really.
