be5a08a0fdc738c6099824743bfe8186f835c065
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-26 16:13:06 -0800

    adding a few projects

---
+++ libs/lightsensor/enums.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum LightCondition {
+    //% block="dark"
+    Dark = 1,  // ANALOG_THRESHOLD_LOW
+    //% block="bright"
+    Bright = 2,  // ANALOG_THRESHOLD_HIGH
+    }
+
+// Auto-generated. Do not edit. Really.
---
+++ libs/microphone/enums.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum LoudnessCondition {
+    //% block="loud"
+    Loud = 2,  // LEVEL_THRESHOLD_HIGH
+    //% block="quiet"
+    Quiet = 1,  // LEVEL_THRESHOLD_LOW
+    }
+
+// Auto-generated. Do not edit. Really.
