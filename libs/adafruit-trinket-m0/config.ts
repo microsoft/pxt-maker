dabbb4e694648e6d25ad9d674d74e2720309efc4
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-27 23:05:19 -0800

    adding jac tx

--- libs/adafruit-trinket-m0/config.ts
+++ libs/adafruit-trinket-m0/config.ts
@@ -26,6 +26,8 @@ namespace config {
     export const PIN_RX = DAL.PA07;
     export const PIN_TX = DAL.PA06;
 
+    export const PIN_JACK_TX = DAL.PA06; // TX
+
     export const PIN_ACCELEROMETER_INT = DAL.PA02; // D1
     export const PIN_ACCELEROMETER_SCL = DAL.PA09; // SCL
     export const PIN_ACCELEROMETER_SDA = DAL.PA08; // SDA    
