d05c464954829f5522347067f36a149a2c11f141
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-29 22:44:21 -0800

    Split jacdac (#106)
    * updated shims
    
    * revert jacdac shims
    
    * adding drivers project
    
    * adding new image
    
    * adding onboard strip to cxp
    
    * updated common packages

--- libs/storage/shims.d.ts
+++ libs/storage/shims.d.ts
@@ -5,7 +5,7 @@ declare namespace storage {
      * Append string data to a new or existing file. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage" 
+    //% parts="storage" 
     //% blockId="storage_append" block="append file $filename with $data" shim=storage::append
     function append(filename: string, data: string): void;
 
@@ -13,14 +13,14 @@ declare namespace storage {
      * Append a buffer to a new or existing file. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage" shim=storage::appendBuffer
+    //% parts="storage" shim=storage::appendBuffer
     function appendBuffer(filename: string, data: Buffer): void;
 
     /** 
      * Overwrite file with string data. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage"
+    //% parts="storage"
     //% blockId="storage_overwrite" block="overwrite file $filename with $data" shim=storage::overwrite
     function overwrite(filename: string, data: string): void;
 
@@ -28,14 +28,14 @@ declare namespace storage {
      * Overwrite file with a buffer. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage" shim=storage::overwriteWithBuffer
+    //% parts="storage" shim=storage::overwriteWithBuffer
     function overwriteWithBuffer(filename: string, data: Buffer): void;
 
     /** 
      * Return true if the file already exists. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage"
+    //% parts="storage"
     //% blockId="storage_exists" block="file $filename exists" shim=storage::exists
     function exists(filename: string): boolean;
 
@@ -43,7 +43,7 @@ declare namespace storage {
      * Delete a file, or do nothing if it doesn't exist. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage"
+    //% parts="storage"
     //% blockId="storage_remove" block="remove file $filename" shim=storage::remove
     function remove(filename: string): void;
 
@@ -51,7 +51,7 @@ declare namespace storage {
      * Return the size of the file, or -1 if it doesn't exists. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage"
+    //% parts="storage"
     //% blockId="storage_size" block="size of file $filename" shim=storage::size
     function size(filename: string): int32;
 
@@ -59,7 +59,7 @@ declare namespace storage {
      * Read contents of file as a string. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage"
+    //% parts="storage"
     //% blockId="storage_read" block="read file $filename" shim=storage::read
     function read(filename: string): string;
 
@@ -67,7 +67,7 @@ declare namespace storage {
      * Read contents of file as a buffer. 
      * @param filename name of the file, eg: "log.txt"
      */
-    //% part="storage" shim=storage::readAsBuffer
+    //% parts="storage" shim=storage::readAsBuffer
     function readAsBuffer(filename: string): Buffer;
 }
 
