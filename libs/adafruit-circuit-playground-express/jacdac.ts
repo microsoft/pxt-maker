93345ac733d5ecd24df7e5a02753d65a30cbd27a
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-19 23:16:24 -0800

    adding other services

--- libs/adafruit-circuit-playground-express/jacdac.ts
+++ libs/adafruit-circuit-playground-express/jacdac.ts
@@ -1,8 +1,22 @@
 namespace jacdac {
     //% fixedInstance whenUsed block="button A service"
-    export const buttonAService = new ButtonService("A", input.buttonA);
+    export const buttonAService = new ButtonService("buttonA", input.buttonA);
     //% fixedInstance whenUsed block="button B service"
-    export const buttonBService = new ButtonService("B", input.buttonB);
+    export const buttonBService = new ButtonService("buttonB", input.buttonB);
     //% fixedInstance whenUsed block="light service"
     export const lightService = new LightService("light", light.pixels);
+    //% fixedInstance whenUsed block="touch A1"
+    export const touchA1 = new TouchButtonService("touchA1", input.touchA1);
+    //% fixedInstance whenUsed block="touch A2"
+    export const touchA2 = new TouchButtonService("touchA2", input.touchA2);
+    //% fixedInstance whenUsed block="touch A3"
+    export const touchA3 = new TouchButtonService("touchA3", input.touchA3);
+    //% fixedInstance whenUsed block="touch A4"
+    export const touchA4 = new TouchButtonService("touchA4", input.touchA4);
+    //% fixedInstance whenUsed block="touch A5"
+    export const touchA5 = new TouchButtonService("touchA5", input.touchA5);
+    //% fixedInstance whenUsed block="touch A6"
+    export const touchA6 = new TouchButtonService("touchA6", input.touchA6);
+    //% fixedInstance whenUsed block="touch A7"
+    export const touchA7 = new TouchButtonService("touchA7", input.touchA7);
 }
