85462a4576e46ca583393b20ea873e905d8311ee
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-26 09:38:01 -0800

    disable jacdac sim until protocol is stabilitzed

--- sim/dalboard.ts
+++ sim/dalboard.ts
@@ -22,7 +22,7 @@ namespace pxsim {
         AccelerometerBoard,
         PixelBoard,
         StorageBoard,
-        JacDacBoard,
+        //JacDacBoard,
         LightSensorBoard,
         TemperatureBoard,
         MicrophoneBoard {
@@ -38,7 +38,7 @@ namespace pxsim {
         touchButtonState: TouchButtonState;
         accelerometerState: AccelerometerState;
         storageState: StorageState;
-        jacdacState: JacDacState;
+        //jacdacState: JacDacState;
         thermometerState: AnalogSensorState;
         thermometerUnitState: TemperatureUnit;
         microphoneState: AnalogSensorState;
@@ -105,7 +105,7 @@ namespace pxsim {
             this._neopixelState = {};
             this.microphoneState = new AnalogSensorState(DAL.DEVICE_ID_MICROPHONE, 52, 120, 75, 96);
             this.storageState = new StorageState();
-            this.jacdacState = new JacDacState(this);
+            //this.jacdacState = new JacDacState(this);
             this.lightSensorState = new AnalogSensorState(DAL.DEVICE_ID_LIGHT_SENSOR, 0, 255, 128 / 4, 896 / 4);
             this.thermometerState = new AnalogSensorState(DAL.DEVICE_ID_THERMOMETER, -20, 50, 10, 30);
             this.thermometerUnitState = TemperatureUnit.Celsius;
