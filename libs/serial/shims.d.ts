bf96264b9416f72664021ef770068a768414748c
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-17 22:58:46 -0800

    Towards enabling latest webusb (#139)
    * twoards enabling latest webusb
    
    * adding uf2 families
    
    * remove global family
    
    * Upgrade codals
    Also, reorder targets so we get all MCUs compiled before their duplicates
    
    * bump pxt
    
    * using latest nrf commit
    
    * more branch overrides
    
    * removing trailing /
    
    * bump nrf
    
    * remove version override
    
    * moving to latest samd21
    
    * reverting latest samd21 changes
    
    * Use the new low-level timers
    
    * Use newer nrf52 codal
    
    * Add cppDependencies
    
    * bump pxt
    
    * bum pxt

--- libs/serial/shims.d.ts
+++ libs/serial/shims.d.ts
@@ -20,7 +20,8 @@ declare namespace serial {
     function setTxBufferSize(size: uint8): void;
 
     /**
-     * Reads a single byte from the serial receive buffer. Negative if error.
+     * Reads a single byte from the serial receive buffer. Negative if error, 0 if no data.
+     * @param asyncRead false to sleep until data arrived; otherwise returns immediately
      */
     //% Group="Read" shim=serial::read
     function read(): int32;
@@ -52,6 +53,9 @@ declare namespace serial {
     /**
     Set the baud rate of the serial port
      */
+    //% weight=10
+    //% blockId=serial_setbaudrate block="serial|set baud rate %rate"
+    //% blockGap=8 inlineInputMode=inline
     //% help=serial/set-baud-rate
     //% group="Configuration" shim=serial::setBaudRate
     function setBaudRate(rate: BaudRate): void;
