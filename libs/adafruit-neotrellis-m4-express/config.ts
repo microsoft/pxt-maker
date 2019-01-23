d8c0e387a06fea0a4818b1fa6e32c9cca76332cb
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-23 13:40:04 -0800

    config dotstars, neopixels

--- libs/adafruit-neotrellis-m4-express/config.ts
+++ libs/adafruit-neotrellis-m4-express/config.ts
@@ -1,24 +1,28 @@
 namespace config {
-export const NUM_NEOPIXELS = 32;
-export const PIN_NEOPIXEL = DAL.PA27;
+    export const PIN_DOTSTAR_DATA = DAL.PB03;
+    export const PIN_DOTSTAR_CLOCK = DAL.PB02;
+    export const NUM_DOTSTARS = 1;
 
-export const PIN_SDA = DAL.PA08;
-export const PIN_SCL = DAL.PA09;
+    export const NUM_NEOPIXELS = 32;
+    export const PIN_NEOPIXEL = DAL.PA27;
 
-export const PIN_MATRIX_KEYPAD_ROW0 = DAL.PA18;
-export const PIN_MATRIX_KEYPAD_ROW1 = DAL.PA19;
-export const PIN_MATRIX_KEYPAD_ROW2 = DAL.PB22;
-export const PIN_MATRIX_KEYPAD_ROW3 = DAL.PB23;
+    export const PIN_SDA = DAL.PA08;
+    export const PIN_SCL = DAL.PA09;
 
-export const PIN_MATRIX_KEYPAD_COL0 = DAL.PA14;
-export const PIN_MATRIX_KEYPAD_COL1 = DAL.PA15;
-export const PIN_MATRIX_KEYPAD_COL2 = DAL.PA16;
-export const PIN_MATRIX_KEYPAD_COL3 = DAL.PA17;
-export const PIN_MATRIX_KEYPAD_COL4 = DAL.PA20;
-export const PIN_MATRIX_KEYPAD_COL5 = DAL.PA21;
-export const PIN_MATRIX_KEYPAD_COL6 = DAL.PA22;
-export const PIN_MATRIX_KEYPAD_COL7 = DAL.PA23;
+    export const PIN_MATRIX_KEYPAD_ROW0 = DAL.PA18;
+    export const PIN_MATRIX_KEYPAD_ROW1 = DAL.PA19;
+    export const PIN_MATRIX_KEYPAD_ROW2 = DAL.PB22;
+    export const PIN_MATRIX_KEYPAD_ROW3 = DAL.PB23;
 
-export const PIN_A0 = DAL.PA02;
-export const PIN_A1 = DAL.PA05;
+    export const PIN_MATRIX_KEYPAD_COL0 = DAL.PA14;
+    export const PIN_MATRIX_KEYPAD_COL1 = DAL.PA15;
+    export const PIN_MATRIX_KEYPAD_COL2 = DAL.PA16;
+    export const PIN_MATRIX_KEYPAD_COL3 = DAL.PA17;
+    export const PIN_MATRIX_KEYPAD_COL4 = DAL.PA20;
+    export const PIN_MATRIX_KEYPAD_COL5 = DAL.PA21;
+    export const PIN_MATRIX_KEYPAD_COL6 = DAL.PA22;
+    export const PIN_MATRIX_KEYPAD_COL7 = DAL.PA23;
+
+    export const PIN_A0 = DAL.PA02;
+    export const PIN_A1 = DAL.PA05;
 }
--- libs/adafruit-neotrellis-m4-express/ns.ts
+++ libs/adafruit-neotrellis-m4-express/ns.ts
@@ -27,3 +27,19 @@ namespace serial {
 namespace loops {
 
 }
+
+namespace light {
+    //% whenUsed
+    export const pixels = light.createStrip(defaultPin(), 32);
+
+    /**
+     * Get the 32 NeoPixel strip
+     */
+    //% help=light/onboard-strip
+    //% blockId="neopixel_onboard_strip" block="onboard strip"
+    //% weight=101 blockGap=8
+    //% blockSetVariable=strip
+    export function onboardStrip(): NeoPixelStrip {
+        return pixels;
+    }
+}
