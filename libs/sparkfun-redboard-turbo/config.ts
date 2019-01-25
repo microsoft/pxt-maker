980ca59bdfe6af5831fb0e14a9b8915bf77fd893
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-24 17:49:45 -0800

    clarify pinout

--- libs/sparkfun-redboard-turbo/config.ts
+++ libs/sparkfun-redboard-turbo/config.ts
@@ -1,6 +1,6 @@
 namespace config {
     export const NUM_NEOPIXELS = 1;
-    export const PIN_NEOPIXEL = DAL.PA30; // check
+    export const PIN_NEOPIXEL = DAL.PA30;
 
     export const PIN_A0 = DAL.PA02;
     export const PIN_A1 = DAL.PB08;
@@ -24,9 +24,10 @@ namespace config {
     export const PIN_D12 = DAL.PA19;
     export const PIN_D13 = DAL.PA17;
 
-    export const PIN_LED = DAL.PA17; // check
-    export const PIN_RX = DAL.PA11; // check
-    export const PIN_TX = DAL.PA10; // check
+    export const PIN_LED = PIN_D13;
+    export const PIN_RX = PIN_D0;
+    export const PIN_TX = PIN_D1;
+
     export const PIN_MISO = DAL.PA12;
     export const PIN_MOSI = DAL.PB10;
     export const PIN_SCK = DAL.PB11;
