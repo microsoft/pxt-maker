63951a6c371e2f872e967d511ef63ab9ad5954ff
Teddy Seyed <t-tesey@microsoft.com>
2019-01-25 17:36:38 -0800

    Fixes for pin issues

--- libs/bead-motion/config.ts
+++ libs/bead-motion/config.ts
@@ -7,6 +7,10 @@ namespace config {
     export const PIN_ACCELEROMETER_SDA = DAL.PA00;
     export const PIN_ACCELEROMETER_SCL = DAL.PA01;
 
+    export const PIN_A5 = DAL.PA08;
+    export const PIN_A6 = DAL.PA09;
+    export const PIN_A7 = DAL.PA10;
+    export const PIN_A8 = DAL.PA11;
 
     export const PIN_D5 = DAL.PA08;
     export const PIN_D6 = DAL.PA09;
--- libs/bead-motion/device.d.ts
+++ libs/bead-motion/device.d.ts
@@ -1,5 +1,14 @@
 declare namespace pins {
     
+    //% fixedInstance shim=pxt::getPin(PIN_A5)
+    const A5: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A6)
+    const A6: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A7)
+    const A7: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A8)
+    const A8: PwmPin;    
+
     //% fixedInstance shim=pxt::getPin(PIN_D5)
     const D5: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_D6)
@@ -11,10 +20,45 @@ declare namespace pins {
 
     //% fixedInstance shim=pxt::getPin(PIN_TX)
     const TX: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_NEOPIXEL)
+    const NEOPIXEL: DigitalInOutPin;
 
 }
 
 declare namespace input {
+
+    /**
+     * Button connecting A5 to GND.
+     */
+    //% block="button A5" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A5,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA5: Button;
+
+    /**
+     * Button connecting A6 to GND.
+     */
+    //% block="button A6" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A6,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA6: Button;
+
+    /**
+     * Button connecting A7 to GND.
+     */
+    //% block="button A7" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A7,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA7: Button;
+
+    /**
+     * Button connecting A8 to GND.
+     */
+    //% block="button A8" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A8,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA8: Button;
+
 
     /**
      * Button connecting D5 to GND.
