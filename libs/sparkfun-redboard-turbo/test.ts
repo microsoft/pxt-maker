9a7353434cbe24558e5263e0c1b7c83a60c702a7
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-29 14:34:31 -0800

    unlink storage until fixed

--- libs/sparkfun-redboard-turbo/test.ts
+++ libs/sparkfun-redboard-turbo/test.ts
@@ -1,6 +1,11 @@
+pins.LED.digitalWrite(true);
+control.dmesg("start")
+
 forever(function() {
+    control.dmesg("on")
     pins.LED.digitalWrite(true);
     pause(500)
+    control.dmesg("off")
     pins.LED.digitalWrite(false);
     pause(500)
 })
