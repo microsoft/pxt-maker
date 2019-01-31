c9883eb1850064886c6101c37294e9297bc799ff
Teddy Seyed <t-tesey@microsoft.com>
2019-01-30 16:56:08 -0800

    Adding in accelerometer!

--- libs/bead-motion/config.ts
+++ libs/bead-motion/config.ts
@@ -6,11 +6,11 @@ namespace config {
     export const PIN_NEOPIXEL = DAL.PB23;
     export const PIN_ACCELEROMETER_SDA = DAL.PA00;
     export const PIN_ACCELEROMETER_SCL = DAL.PA01;
-    export const PIN_ACCELEROMETER_INT = DAL.PA08; // D5 test
+    export const PIN_ACCELEROMETER_INT = DAL.PA13; // D5 test
 
     export const PIN_A5 = DAL.PA08;
     export const PIN_A6 = DAL.PA09;
-    export const PIN_A7 = DAL.PA10;
+    export const PIN_A7 = DAL.PA10
     export const PIN_A8 = DAL.PA11;
 
     export const PIN_D5 = DAL.PA08;
