cf4db657591d242211d9e49485d8ed9e5bc10964
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-01 22:52:28 -0800

    udpated shims

--- libs/serial/shims.d.ts
+++ libs/serial/shims.d.ts
@@ -2,6 +2,53 @@
 declare namespace serial {
 
     /**
+     * Opens a Serial communication driver
+     */
+    //% parts=serial shim=serial::createSerial
+    function createSerial(tx: DigitalInOutPin, rx: DigitalInOutPin): SerialDevice;
+}
+
+
+declare interface SerialDevice {
+    /**
+     * Sets the size of the RX buffer in bytes
+     */
+    //% shim=SerialDeviceMethods::setRxBufferSize
+    setRxBufferSize(size: uint8): void;
+
+    /**
+     * Sets the size of the TX buffer in bytes
+     */
+    //% shim=SerialDeviceMethods::setTxBufferSize
+    setTxBufferSize(size: uint8): void;
+
+    /**
+    Set the baud rate of the serial port
+     */
+    //% shim=SerialDeviceMethods::setBaudRate
+    setBaudRate(rate: BaudRate): void;
+
+    /**
+     * Reads a single byte from the serial receive buffer. Negative if error, 0 if no data.
+     */
+    //% shim=SerialDeviceMethods::read
+    read(): int32;
+
+    /**
+     * Read the buffered received data as a buffer
+     */
+    //% shim=SerialDeviceMethods::readBuffer
+    readBuffer(): Buffer;
+
+    /**
+     * Send a buffer across the serial connection.
+     */
+    //% shim=SerialDeviceMethods::writeBuffer
+    writeBuffer(buffer: Buffer): void;
+}
+declare namespace serial {
+
+    /**
      * Sets the size of the RX buffer in bytes
      */
     //% help=serial/set-rx-buffer-size
