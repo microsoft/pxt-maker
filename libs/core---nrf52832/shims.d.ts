53aa5646bbf78d49b8e1e6a66fb708691d38a865
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-04 16:05:02 -0800

    I2c device (#172)
    * updated shims
    
    * regor pins
    
    * updated shims
    
    * move more out of C++
    
    * bump pxt

--- libs/core---nrf52832/shims.d.ts
+++ libs/core---nrf52832/shims.d.ts
@@ -233,74 +233,36 @@ declare namespace pins {
     /**
      * Opens a SPI driver
      */
+    //% help=pins/create-spi
     //% parts=spi shim=pins::createSPI
-    function createSPI(mosiPin: DigitalInOutPin, misoPin: DigitalInOutPin, sckPin: DigitalInOutPin): SPIDevice;
+    function createSPI(mosiPin: DigitalInOutPin, misoPin: DigitalInOutPin, sckPin: DigitalInOutPin): SPI;
 }
 
 
-declare interface SPIDevice {
+declare interface SPI {
     /**
      * Write to the SPI bus
      */
-    //% shim=SPIDeviceMethods::write
+    //% shim=SPIMethods::write
     write(value: int32): int32;
 
     /**
      * Transfer buffers over the SPI bus
      */
-    //% shim=SPIDeviceMethods::transfer
+    //% shim=SPIMethods::transfer
     transfer(command: Buffer, response: Buffer): void;
 
     /**
      * Sets the SPI clock frequency
      */
-    //% shim=SPIDeviceMethods::setFrequency
+    //% shim=SPIMethods::setFrequency
     setFrequency(frequency: int32): void;
 
     /**
      * Sets the SPI bus mode
      */
-    //% shim=SPIDeviceMethods::setMode
+    //% shim=SPIMethods::setMode
     setMode(mode: int32): void;
-}
-declare namespace pins {
-
-    /**
-     * Gets the default SPI driver
-     */
-    //% shim=pins::spi
-    function spi(): SPIDevice;
-
-    /**
-     * Write to the SPI slave and return the response
-     * @param value Data to be sent to the SPI slave
-     */
-    //% help=pins/spi-write weight=5 advanced=true
-    //% blockId=spi_write block="spi write %value" shim=pins::spiWrite
-    function spiWrite(value: int32): int32;
-
-    /**
-     * Writes a given command to SPI bus, and afterwards reads the response.
-     */
-    //% help=pins/spi-transfer weight=4 advanced=true
-    //% blockId=spi_transfer block="spi transfer %command into %response" shim=pins::spiTransfer
-    function spiTransfer(command: Buffer, response: Buffer): void;
-
-    /**
-     * Sets the SPI frequency
-     * @param frequency the clock frequency, eg: 1000000
-     */
-    //% help=pins/spi-frequency weight=4 advanced=true
-    //% blockId=spi_frequency block="spi frequency %frequency" shim=pins::spiFrequency
-    function spiFrequency(frequency: int32): void;
-
-    /**
-     * Sets the SPI mode and bits
-     * @param mode the mode, eg: 3
-     */
-    //% help=pins/spi-mode weight=3 advanced=true
-    //% blockId=spi_mode block="spi mode %mode" shim=pins::spiMode
-    function spiMode(mode: int32): void;
 }
 
 // Auto-generated. Do not edit. Really.
--- libs/core---samd/shims.d.ts
+++ libs/core---samd/shims.d.ts
@@ -228,93 +228,65 @@ declare namespace control {
     //% shim=control::dmesgPtr
     function dmesgPtr(str: string, ptr: Object): void;
 }
-declare namespace pins {
 
+
+declare interface I2C {
     /**
      * Read `size` bytes from a 7-bit I2C `address`.
      */
-    //% repeat.defl=0 shim=pins::i2cReadBuffer
-    function i2cReadBuffer(address: int32, size: int32, repeat?: boolean): Buffer;
+    //% repeat.defl=0 shim=I2CMethods::readBuffer
+    readBuffer(address: int32, size: int32, repeat?: boolean): Buffer;
 
     /**
      * Write bytes to a 7-bit I2C `address`.
      */
-    //% repeat.defl=0 shim=pins::i2cWriteBuffer
-    function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+    //% repeat.defl=0 shim=I2CMethods::writeBuffer
+    writeBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+}
+declare namespace pins {
+
+    /**
+     * Opens a Serial communication driver
+     */
+    //% help=pins/create-i2c
+    //% parts=i2c shim=pins::createI2C
+    function createI2C(sda: DigitalInOutPin, scl: DigitalInOutPin): I2C;
 }
 declare namespace pins {
 
     /**
      * Opens a SPI driver
      */
+    //% help=pins/create-spi
     //% parts=spi shim=pins::createSPI
-    function createSPI(mosiPin: DigitalInOutPin, misoPin: DigitalInOutPin, sckPin: DigitalInOutPin): SPIDevice;
+    function createSPI(mosiPin: DigitalInOutPin, misoPin: DigitalInOutPin, sckPin: DigitalInOutPin): SPI;
 }
 
 
-declare interface SPIDevice {
+declare interface SPI {
     /**
      * Write to the SPI bus
      */
-    //% shim=SPIDeviceMethods::write
+    //% shim=SPIMethods::write
     write(value: int32): int32;
 
     /**
      * Transfer buffers over the SPI bus
      */
-    //% shim=SPIDeviceMethods::transfer
+    //% shim=SPIMethods::transfer
     transfer(command: Buffer, response: Buffer): void;
 
     /**
      * Sets the SPI clock frequency
      */
-    //% shim=SPIDeviceMethods::setFrequency
+    //% shim=SPIMethods::setFrequency
     setFrequency(frequency: int32): void;
 
     /**
      * Sets the SPI bus mode
      */
-    //% shim=SPIDeviceMethods::setMode
+    //% shim=SPIMethods::setMode
     setMode(mode: int32): void;
-}
-declare namespace pins {
-
-    /**
-     * Gets the default SPI driver
-     */
-    //% shim=pins::spi
-    function spi(): SPIDevice;
-
-    /**
-     * Write to the SPI slave and return the response
-     * @param value Data to be sent to the SPI slave
-     */
-    //% help=pins/spi-write weight=5 advanced=true
-    //% blockId=spi_write block="spi write %value" shim=pins::spiWrite
-    function spiWrite(value: int32): int32;
-
-    /**
-     * Writes a given command to SPI bus, and afterwards reads the response.
-     */
-    //% help=pins/spi-transfer weight=4 advanced=true
-    //% blockId=spi_transfer block="spi transfer %command into %response" shim=pins::spiTransfer
-    function spiTransfer(command: Buffer, response: Buffer): void;
-
-    /**
-     * Sets the SPI frequency
-     * @param frequency the clock frequency, eg: 1000000
-     */
-    //% help=pins/spi-frequency weight=4 advanced=true
-    //% blockId=spi_frequency block="spi frequency %frequency" shim=pins::spiFrequency
-    function spiFrequency(frequency: int32): void;
-
-    /**
-     * Sets the SPI mode and bits
-     * @param mode the mode, eg: 3
-     */
-    //% help=pins/spi-mode weight=3 advanced=true
-    //% blockId=spi_mode block="spi mode %mode" shim=pins::spiMode
-    function spiMode(mode: int32): void;
 }
 
 // Auto-generated. Do not edit. Really.
