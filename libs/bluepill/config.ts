0bd68d0f9d0caddf9718591f7ffebda69f49d0ae
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-26 16:09:51 -0800

    updated config

--- libs/bluepill/config.ts
+++ libs/bluepill/config.ts
@@ -1,11 +1,45 @@
 
 namespace config {
-    export const PIN_SCK = DAL.PA_5;
-    export const PIN_MISO = DAL.PA_6;
-    export const PIN_MOSI = DAL.PA_7;
+    export const PIN_A0 = DAL.PA_0;
+    export const PIN_A1 = DAL.PA_1;
+    export const PIN_A2 = DAL.PA_2;
+    export const PIN_A3 = DAL.PA_3;
+    export const PIN_A4 = DAL.PA_4;
+    export const PIN_A5 = DAL.PA_5;
+    export const PIN_A6 = DAL.PA_6;
+    export const PIN_A7 = DAL.PA_7;
+    export const PIN_A8 = DAL.PA_8;
+    export const PIN_A9 = DAL.PA_9;
+    export const PIN_A10 = DAL.PA_10;
+    export const PIN_A11 = DAL.PA_11;
+    export const PIN_A12 = DAL.PA_12;
+    export const PIN_A15 = DAL.PA_15;
 
-    export const PIN_RX = DAL.PA_10;
-    export const PIN_TX = DAL.PA_9;
+    export const PIN_D0 = DAL.PB_0;
+    export const PIN_D1 = DAL.PB_1;
+    export const PIN_D2 = DAL.PB_2;
+    export const PIN_D3 = DAL.PB_3;
+    export const PIN_D4 = DAL.PB_4;
+    export const PIN_D5 = DAL.PB_5;
+    export const PIN_D6 = DAL.PB_6;
+    export const PIN_D7 = DAL.PB_7;
+    export const PIN_D8 = DAL.PB_8;
+    export const PIN_D9 = DAL.PB_9;
+    export const PIN_D10 = DAL.PB_10;
+    export const PIN_D11 = DAL.PB_11;
+    export const PIN_D12 = DAL.PB_12;
+    export const PIN_D13 = DAL.PB_13;
+    export const PIN_D14 = DAL.PB_14;
+    export const PIN_D15 = DAL.PB_15;
+
+    export const PIN_SCK = PIN_A5;
+    export const PIN_MISO = PIN_A6;
+    export const PIN_MOSI = PIN_A7;
+
+    export const PIN_RX = PIN_10;
+    export const PIN_TX = PIN_A9;
+
+    export const JACK_TX = PIN_TX;
 
     export const PIN_SDA = DAL.PB_7;
     export const PIN_SCL = DAL.PB_6;
--- libs/bluepill/device.d.ts
+++ libs/bluepill/device.d.ts
@@ -1,5 +1,67 @@
 // TODO remove pins?
 declare namespace pins {
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A0)
+    const A0: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A1)
+    const A1: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A2)
+    const A2: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A3)
+    const A3: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A4)
+    const A4: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A5)
+    const A5: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A6)
+    const A6: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A7)
+    const A7: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A8)
+    const A8: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A9)
+    const A9: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A10)
+    const A10: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A11)
+    const A11: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A12)
+    const A12: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A15)
+    const A15: DigitalInOutPin;
+
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D0)
+    const B0: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D1)
+    const B1: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D2)
+    const B2: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D3)
+    const B3: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D4)
+    const B4: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D5)
+    const B5: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D6)
+    const B6: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D7)
+    const B7: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D8)
+    const B8: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D9)
+    const B9: PwmOnlyPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D10)
+    const B10: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D11)
+    const B11: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D12)
+    const B12: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D13)
+    const B13: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D14)
+    const B14: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D15)
+    const B15: DigitalInOutPin;
+
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_LED)
     const LED: DigitalInOutPin;
 
@@ -20,4 +82,3 @@ declare namespace pins {
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_TX)
     const TX: DigitalInOutPin;
 }
-
