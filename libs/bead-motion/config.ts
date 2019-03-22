c51a6dbe29b1cec6018352fda225015699bff44e
Teddy Seyed <t-tesey@microsoft.com>
2019-03-21 19:04:33 -0700

    Fixing the config for motion bead v2

--- libs/bead-motion/config.ts
+++ libs/bead-motion/config.ts
@@ -1,24 +1,31 @@
 
 namespace config {
 
-    export const NUM_NEOPIXELS = 1;
-
-    export const PIN_NEOPIXEL = DAL.PB23;
     export const PIN_ACCELEROMETER_SDA = DAL.PA00;
     export const PIN_ACCELEROMETER_SCL = DAL.PA01;
     export const PIN_ACCELEROMETER_INT = DAL.PA13;
 
-    export const PIN_A1 = DAL.PA08;
-    export const PIN_A2 = DAL.PA09;
-    export const PIN_A3 = DAL.PA10;
-    export const PIN_A4 = DAL.PA11;
+    export const PIN_A0 = DAL.PA02; 
+    export const PIN_A1 = DAL.PA05; 
+    export const PIN_A2 = DAL.PA06; 
+    export const PIN_A3 = DAL.PA07; 
+    export const PIN_A4 = DAL.PB03; 
+    export const PIN_A5 = DAL.PB02; 
 
-    export const PIN_D1 = DAL.PA08;
-    export const PIN_D2 = DAL.PA09;
-    export const PIN_D3 = DAL.PA10;
-    export const PIN_D4 = DAL.PA11;
+    export const PIN_D0 = DAL.PA02; 
+    export const PIN_D1 = DAL.PA05; 
+    export const PIN_D2 = DAL.PA06; 
+    export const PIN_D3 = DAL.PA07; 
+    export const PIN_D4 = DAL.PB03; 
+    export const PIN_D5 = DAL.PB02; 
 
-    export const PIN_TX = DAL.PB08;  
+    export const PIN_SDA = PIN_D5;
+    export const PIN_SCL = PIN_D4;
+     
+    export const PIN_LED = PIN_D13;
+    export const PIN_RXLED = DAL.PA18;    
+
+    export const PIN_TX = DAL.PB08;
     export const PIN_JACK_TX = PIN_TX;
 
 }
