6726b617b3b30090a91f47ab6b49b3c38c30ff54
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-05 14:00:52 -0700

    add link

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 npm install
+rm -Rf node_modules/@jacdac/jacdac-ts
 rm -Rf node_modules/pxt-core
 rm -Rf node_modules/pxt-common-packages
+pxt link ../jacdac-ts
 pxt link ../pxt
 pxt link ../pxt-common-packages
