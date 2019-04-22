87e579b157d445046df100b95dc1702e89ec9eb4
James Devine <j.devine@lancaster.ac.uk>
2019-04-22 15:56:43 -0700

    don't force remove. (rm)

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 npm install
-rm -Rf node_modules/pxt-core
-rm -Rf node_modules/pxt-common-packages
-rm -Rf node_modules/@jacdac/jacdac-ts
+rm -R node_modules/pxt-core
+rm -R node_modules/pxt-common-packages
+rm -R node_modules/@jacdac/jacdac-ts
 pxt link ../pxt
 pxt link ../pxt-common-packages
 pxt link ../jacdac-ts
