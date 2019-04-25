3632c314cae8a6e1b813925269e8bdb720e1f85a
James Devine <j.devine@lancaster.ac.uk>
2019-04-24 17:12:48 -0700

    fix prox bead and jacdac service init

---
+++ libs/bead-tattoo/jacdac.ts
@@ -0,0 +1,14 @@
+namespace jacdac {
+    //% fixedInstance whenUsed block="touch A0"
+    export const touchA0 = new jacdac.TouchButtonService("touchA0", input.touchA0);
+    //% fixedInstance whenUsed block="touch A1"
+    export const touchA1 = new jacdac.TouchButtonService("touchA1", input.touchA1);
+    //% fixedInstance whenUsed block="touch A2"
+    export const touchA2 = new jacdac.TouchButtonService("touchA2", input.touchA2);
+    //% fixedInstance whenUsed block="touch A3"
+    export const touchA3 = new jacdac.TouchButtonService("touchA3", input.touchA3);
+    //% fixedInstance whenUsed block="touch A4"
+    export const touchA4 = new jacdac.TouchButtonService("touchA4", input.touchA4);
+    //% fixedInstance w5enUsed block="touch A5"
+    export const touchA5 = new jacdac.TouchButtonService("touchA5", input.touchA5);
+}
--- libs/bead-tattoo/startup.ts
+++ libs/bead-tattoo/startup.ts
@@ -1,3 +1,9 @@
 // start exposing accelerometer + touch
 jacdac.consoleService();
 jacdac.accelerometerService.start();
+jacdac.touchA0.start();
+jacdac.touchA1.start();
+jacdac.touchA2.start();
+jacdac.touchA3.start();
+jacdac.touchA4.start();
+jacdac.touchA5.start();
