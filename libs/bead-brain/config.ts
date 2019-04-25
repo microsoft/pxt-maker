e1d5cd4e445b48635feca4785771382a9889aa77
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-25 09:30:13 -0700

    reorg pins

--- libs/bead-brain/config.ts
+++ libs/bead-brain/config.ts
@@ -6,7 +6,7 @@ namespace config {
     export const PIN_ACCELEROMETER_SCL = DAL.PA01;
     export const PIN_ACCELEROMETER_INT = DAL.PA13; 
 
-    export const PIN_A1 = DAL.PA07;
+    export const PIN_A1 = DAL.PA07; // J18
     export const PIN_A2 = DAL.PA06;
     export const PIN_A3 = DAL.PA05;
     export const PIN_A4 = DAL.PB08;
@@ -14,15 +14,15 @@ namespace config {
     export const PIN_A6 = DAL.PA04;
     export const PIN_A7 = DAL.PB03;
 
-    export const PIN_D1 = DAL.PA07;
-    export const PIN_D2 = DAL.PA06;
-    export const PIN_D3 = DAL.PA05;
-    export const PIN_D4 = DAL.PB08;
-    export const PIN_D5 = DAL.PB09;
-    export const PIN_D6 = DAL.PA04;
-    export const PIN_D7 = DAL.PB03;
-    export const PIN_D13 = DAL.PA17;
+    export const PIN_D1 = PIN_A1;
+    export const PIN_D2 = PIN_A2;
+    export const PIN_D3 = PIN_A3;
+    export const PIN_D4 = PIN_A4;
+    export const PIN_D5 = PIN_A5;
+    export const PIN_D6 = PIN_A6;
+    export const PIN_D7 = PIN_A7;
 
+    export const PIN_D13 = DAL.PA17;
     export const PIN_TX = DAL.PB08; 
 
     export const PIN_LED = PIN_D13;
