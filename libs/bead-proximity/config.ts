d40b89dd044317fb6e4843d12e5ac35a23ec0eb1
James Devine <j.devine@lancaster.ac.uk>
2019-04-29 19:03:16 -0400

    fix bead-proximity i2c pins

--- libs/bead-proximity/config.ts
+++ libs/bead-proximity/config.ts
@@ -1,5 +1,8 @@
 namespace config {
 
+    export const PIN_SDA = DAL.PA00;
+    export const PIN_SCL = DAL.PA01;
+
     export const PIN_A0 = DAL.PA02;
     export const PIN_A1 = DAL.PA05;
     export const PIN_A2 = DAL.PA06;
@@ -14,9 +17,6 @@ namespace config {
     export const PIN_D4 = DAL.PB03;
     export const PIN_D5 = DAL.PB02;
     export const PIN_D13 = DAL.PA17;
-
-    export const PIN_SDA = PIN_D5;
-    export const PIN_SCL = PIN_D4;
 
     export const PIN_LED = PIN_D13;
     export const PIN_JACK_COMMLED = 0x10012; // top bit is active lo flag, 12 is pin 18 (GREEN)
