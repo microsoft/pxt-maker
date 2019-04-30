bdc5b87830d91d46324753d1e8e73f78d4abd252
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-30 15:33:13 -0700

    updated tattoo apis

--- libs/bead-tattoo/startup.ts
+++ libs/bead-tattoo/startup.ts
@@ -1,8 +1,3 @@
 jacdac.consoleService();
 //jacdac.accelerometerService.start();
-jacdac.touchA0.start();
-jacdac.touchA1.start();
-jacdac.touchA2.start();
-jacdac.touchA3.start();
-jacdac.touchA4.start();
-jacdac.touchA5.start();
+jacdac.touchButtonsService.start();
