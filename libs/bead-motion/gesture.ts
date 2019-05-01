84ea0a45cfe6d3503a90c58ecfd1d1d245f31165
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-30 22:01:36 -0700

    support for custom gestures

---
+++ libs/bead-motion/gesture.ts
@@ -0,0 +1,25 @@
+const enum BeadGesture {
+    Step
+}
+
+namespace input {
+    function stepUpdate(): () => boolean {
+        let active = true;
+        return function() {
+            const s = input.acceleration(Dimension.Strength);        
+            if (!active && s > 2000) {
+                active = true;
+                return true;
+            } else if (s < 1500) {
+                active = false;
+            }
+            return false;
+        }
+    }
+
+    function stepHandler(): void {
+        jacdac.accelerometerService.raiseCustomGestureEvent(BeadGesture.Step);
+    }
+
+    input.onCustomGesture(BeadGesture.Step, stepUpdate(), stepHandler); // register
+}
