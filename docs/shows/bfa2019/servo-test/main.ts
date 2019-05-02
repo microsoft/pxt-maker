dc89d4a9c8219fb5defb23378b22a4b67b4f1e75
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-02 13:50:55 -0700

    updated servo test

--- docs/shows/bfa2019/servo-test/main.ts
+++ docs/shows/bfa2019/servo-test/main.ts
@@ -4,30 +4,27 @@ namespace jacdac {
 }
 
 const tattoo = jacdac.touchButtonsClient;
-const ser = jacdac.servosClient;
+const servo1 = new servos.PinServo(pins.A2);
+const servo2 = new servos.PinServo(pins.A3);
+const l = light.pixels5;
 
-light.pixels.setAll(0x00ffff)
-for(let i = 0; i < 2; ++i) {
-    ser.setAngle(0, 180)
-    pause(100)
-    ser.setAngle(0, 0);
-    pause(2000)
-}
-ser.setAngle(0, 90);
-ser.setAngle(1, 90);
+let drift1 = -5;
+let drift2 = 3;
+//l.setAll(0x00ffff)
+
 
 tattoo.onEvent(0, JDButtonEvent.Down, function() {
-    ser.setAngle(0, 180);
-    ser.setAngle(1, 180);
-    light.pixels.setAll(0xff0000)
+    servo1.run(80 + drift1);
+    servo2.run(80 + drift2);
+//    l.setAll(0xff0000)
 })
 tattoo.onEvent(1, JDButtonEvent.Down, function() {
-    ser.setAngle(0, 0);
-    ser.setAngle(1, 0);
-    light.pixels.setAll(0x0000ff)
+    servo1.run(0 + drift1);
+    servo2.run(0 + drift2);
+  //  l.setAll(0x0000ff)
 })
 tattoo.onEvent(2, JDButtonEvent.Down, function() {
-    ser.setAngle(0, 90);
-    ser.setAngle(1, 90);
-    light.pixels.setAll(0x00ff00)
+    servo1.run(-80 + drift1);
+    servo2.run(-80 + drift2);
+    //l.setAll(0x00ff00)
 })
