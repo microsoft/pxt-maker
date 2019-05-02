23a75cb0d3b05931dd0c1b7b4b2b34f6bc1b76b6
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-02 11:38:28 -0700

    updated servo test

--- docs/shows/bfa2019/servo-test/main.ts
+++ docs/shows/bfa2019/servo-test/main.ts
@@ -5,12 +5,28 @@ namespace jacdac {
 
 const tattoo = jacdac.touchButtonsClient;
 
+light.pixels.setAll(0x00ffff)
+for(let i = 0; i < 2; ++i) {
+    jacdac.servosClient.setAngle(0, 180)
+    pause(100)
+    jacdac.servosClient.setAngle(0, 0);
+    pause(2000)
+}
+jacdac.servosClient.setAngle(0, 90);
+jacdac.servosClient.setAngle(1, 90);
+
 tattoo.onEvent(0, JDButtonEvent.Down, function() {
-    jacdac.servosClient.run(0, 100);
+    jacdac.servosClient.setAngle(0, 180);
+    jacdac.servosClient.setAngle(1, 180);
+    light.pixels.setAll(0xff0000)
 })
 tattoo.onEvent(1, JDButtonEvent.Down, function() {
-    jacdac.servosClient.run(0, -100);
+    jacdac.servosClient.setAngle(0, 0);
+    jacdac.servosClient.setAngle(1, 0);
+    light.pixels.setAll(0x0000ff)
 })
 tattoo.onEvent(2, JDButtonEvent.Down, function() {
-    jacdac.servosClient.run(0, 0);
+    jacdac.servosClient.setAngle(0, 90);
+    jacdac.servosClient.setAngle(1, 90);
+    light.pixels.setAll(0x00ff00)
 })
