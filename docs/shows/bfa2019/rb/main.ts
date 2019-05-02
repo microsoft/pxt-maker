1e90fa25f52bf6a407fcd41d271a58f562de4435
Tom Ball <tballmsft@users.noreply.github.com>
2019-05-02 14:27:28 -0700

    get rebecca's code working (#226)

--- docs/shows/bfa2019/rb/main.ts
+++ docs/shows/bfa2019/rb/main.ts
@@ -4,16 +4,23 @@
 const tattoo = jacdac.touchButtonsClient;
 const lights = light.pixels;
 lights.setBrightness(200);
+lights.setLength(50);
 
 // actions
 let dripping = false;
+let countdown = 0;
 function drip() {
     if (!dripping) {
+        countdown = 0;
+        lights.clear()
         dripping = true;
-        for (let i = 0; i < 30; ++i) {
-            if (i % 3)
+        for (let i = 0; i < 100; ++i) {
+            if (countdown == 0) {
                 lights.setPixelColor(0, Colors.White);
-            lights.move(LightMove.Rotate, 1);
+                countdown = Math.randomRange(1,10);
+            } else 
+                countdown--;
+            lights.move(LightMove.Shift, 1);
             pause(100);
         }
         dripping = false
@@ -21,6 +28,6 @@ function drip() {
 }
 
 // events
-tattoo.onEvent(0, JDButtonEvent.Down, function () {
+tattoo.onEvent(2, JDButtonEvent.Down, function () {
     drip();
 })
