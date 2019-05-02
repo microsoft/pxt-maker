5a7030ce489b9c17a265d11c531d5fc8852f042f
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-02 14:26:10 -0700

    servos working

--- docs/shows/bfa2019/om/main.ts
+++ docs/shows/bfa2019/om/main.ts
@@ -2,39 +2,63 @@
 
 // hardware
 const tattoo = jacdac.touchButtonsClient;
-const servos = new jacdac.ServosClient("servoes", 2);
+const servo2 = servos.servo2;
+const servo3 = servos.servo3;
+const drift2 = -5;
+const drift3 = 3;
+
 const duration = 3000;
 
 // actions
-enum State {
-    Idle,
-    Up,
-    Down
-}
-let state = State.Idle;
-function up() {
-    if (state == State.Idle) {
-        state = State.Up;
-        servos.run(0, 50);
-        servos.run(1, 50);
-        pause(duration);
-        state = State.Idle;
+let idle = true;
+let isup = false;
+function toggle() {
+    if (!idle) return;
+
+    idle = false;
+    if (!isup) {
+        moveUp();
+    } else {
+        moveDown();
     }
+    pause(duration);
+    stop();
+    isup = !isup;
+    idle = true;
 }
 function down() {
-    if (state == State.Idle) {
-        state = State.Down;
-        servos.run(0, -50);
-        servos.run(1, -50);
-        pause(duration);
-        state = State.Idle;
+    if (idle && isup) {
     }
+}
+
+function moveUp() {
+    servo2.run(drift2 + 100);
+    servo3.run(drift3 + 100);
+}
+
+function moveDown() {
+    servo2.run(drift2 - 100);
+    servo3.run(drift3 - 100);
+}
+
+function stop() {
+    servo2.run(drift2 + 0);
+    servo3.run(drift3 + 0);
 }
 
 // events
 tattoo.onEvent(0, JDButtonEvent.Down, function () {
-    up();
+    toggle();
 })
-tattoo.onEvent(1, JDButtonEvent.Down, function () {
-    down();
+tattoo.onEvent(2, JDButtonEvent.Down, function () {
+    moveUp();
+});
+tattoo.onEvent(2, JDButtonEvent.Up, function () {
+    stop();
+})
+tattoo.onEvent(3, JDButtonEvent.Down, function () {
+    moveDown();
+})
+tattoo.onEvent(3, JDButtonEvent.Up, function () {
+    stop();
 })
--- docs/shows/bfa2019/servo-test/main.ts
+++ docs/shows/bfa2019/servo-test/main.ts
@@ -1,11 +1,6 @@
-namespace jacdac {
-    //% fixedInstance
-    export const servosClient = new jacdac.ServosClient("servos", 2);
-}
-
 const tattoo = jacdac.touchButtonsClient;
-const servo1 = new servos.PinServo(pins.A2);
-const servo2 = new servos.PinServo(pins.A3);
+const servo2 = servos.servo2;
+const servo3 = servos.servo3;
 const l = light.pixels5;
 
 let drift1 = -5;
@@ -14,17 +9,17 @@ let drift2 = 3;
 
 
 tattoo.onEvent(0, JDButtonEvent.Down, function() {
-    servo1.run(80 + drift1);
-    servo2.run(80 + drift2);
+    servo2.run(80 + drift1);
+    servo3.run(80 + drift2);
 //    l.setAll(0xff0000)
 })
 tattoo.onEvent(1, JDButtonEvent.Down, function() {
-    servo1.run(0 + drift1);
-    servo2.run(0 + drift2);
+    servo2.run(0 + drift1);
+    servo3.run(0 + drift2);
   //  l.setAll(0x0000ff)
 })
 tattoo.onEvent(2, JDButtonEvent.Down, function() {
-    servo1.run(-80 + drift1);
-    servo2.run(-80 + drift2);
+    servo2.run(-80 + drift1);
+    servo3.run(-80 + drift2);
     //l.setAll(0x00ff00)
 })
