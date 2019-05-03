edd6ffb7ced6ca68c491190fc3af742d49987fe0
Tom Ball <tball@microsoft.com>
2019-05-02 21:30:20 -0700

    copy zipora over to kt

---
+++ docs/shows/bfa2019/kt/main.ts
@@ -0,0 +1,35 @@
+// KT
+
+// hardware
+const lights1 = light.pixels;
+const lights2 = light.pixels2;
+
+// actions
+function pulse() {
+
+}
+
+lights1.setLength(46);
+lights1.setBrightness(0);
+lights1.setAll(0xffffff);
+lights2.setLength(46);
+lights2.setBrightness(0);
+lights2.setAll(0xffffff);
+
+//const testLights = light.pixels
+//testLights.setLength(20);
+const motionLeft = new jacdac.AccelerometerClient("step");
+const motionRight = jacdac.accelerometerClient;
+
+motionLeft.requiredDeviceName = "L"
+motionRight.requiredDeviceName = "R"
+
+motionLeft.onCustomGesture(BeadGesture.Step, function () {
+    //testLights.setAll(0xff0000)
+    lights1.startBrightnessTransition(80, 0, 1000);
+})
+
+motionRight.onCustomGesture(BeadGesture.Step, function () {
+    //testLights.setAll(0x0000ff)
+    lights2.startBrightnessTransition(80, 0, 1000);
+})
