52ed6f511aa71e3733122ebf2bfe3cdde092dda1
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-03 10:48:48 -0700

    base animation

--- docs/shows/bfa2019/op/main.ts
+++ docs/shows/bfa2019/op/main.ts
@@ -2,56 +2,18 @@
 
 // hardware
 const lights = light.pixels;
-lights.setBrightness(25);
+lights.setBrightness(255);
 lights.setLength(288)
 const lights2 = light.pixels2;
 lights2.setBrightness(25);
 lights2.setLength(120)
 
-const beam = lights.range(0,40);
-beam.setAll(0xff0000);
+lights2.startBrightnessTransition(200, 0, 1000, -1, false,
+    new light.EasingBrightnessTransition(easing.outCubic)
+);
 
-const beam2 = lights2.range(0,40);
-beam2.setAll(0x0000ff)
-control.runInBackground(function(){
-    while(true) {
-        lights.move(LightMove.Rotate);
-        pause(1);    
-    }
+let sparkle = new light.SparkleAnimation(0xff,0,0, 30);
+
+forever(function() {
+    lights.showAnimation(sparkle, 500);
 })
-
-control.runInBackground(function(){
-    while(true) {
-        lights2.move(LightMove.Rotate);
-        pause(1);    
-    }
-})
-
-
-// lights.setBrightness(0);
-// lights.setAll(0xff00c0);
-// lights.setLength(30);
-// const promixity = jacdac.proximityClient;
-// const motion = jacdac.accelerometerClient;
-
-// // actions
-// let on = false
-// function pulse() {
-//     if (!on) {
-//         lights.startBrightnessTransition(0, 80, 2000, 2, true,
-//             new light.EasingBrightnessTransition(easing.linear, easing.linear)
-//         );
-//     } else {
-//         lights.clear()
-//     }
-//     on = !on;
-// }
-
-// // events
-// promixity.onEvent(JDPromixityEvent.Close, function () {
-//     pulse();
-// })
-
-//motion.onCustomGesture(BeadGesture.Step, function () {
-//    pulse();
-//})
