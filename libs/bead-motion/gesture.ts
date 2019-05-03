2a8eb55ae89f9dc4fb308a3a405d360f88c70bac
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-03 12:43:29 -0700

    adding lightstep

--- libs/bead-motion/gesture.ts
+++ libs/bead-motion/gesture.ts
@@ -18,4 +18,24 @@ namespace input {
     }
 
     input.onCustomGesture(BeadGesture.Step, stepUpdate(), stepHandler); // register
+
+    function lightStepUpdate(): () => boolean {
+        let active = true;
+        return function() {
+            const s = input.acceleration(Dimension.Strength);        
+            if (!active && s > 1500) {
+                active = true;
+                return true;
+            } else if (s < 1200) {
+                active = false;
+            }
+            return false;
+        }
+    }
+
+    function lightStepHandler(): void {
+        jacdac.accelerometerService.raiseCustomGestureEvent(BeadGesture.LightStep);
+    }
+
+    input.onCustomGesture(BeadGesture.LightStep, lightStepUpdate(), lightStepHandler); // register
 }
