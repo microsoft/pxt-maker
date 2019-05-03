218fe9e35372a840d9f34cc4cc451adb2764b891
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-03 12:21:04 -0700

    updated ds

--- docs/shows/bfa2019/ds/main.ts
+++ docs/shows/bfa2019/ds/main.ts
@@ -14,31 +14,49 @@ const touchLeft = new jacdac.TouchButtonsClient("touch");
 const touchRight = jacdac.touchButtonsClient;
 touchLeft.requiredDeviceName = "L"
 touchRight.requiredDeviceName = "R"
-touchLeft.onConnected = function() {
+touchLeft.onConnected = function () {
     touchLeft.calibrate()
 }
-touchRight.onConnected = function() {
+touchRight.onConnected = function () {
     touchRight.calibrate()
 }
 const rainbowAnimation: light.NeoPixelAnimation = new light.RainbowCycleAnimation(50)
-const runningAnimation: light.NeoPixelAnimation = new light.RunningLightsAnimation(0xFF,0,0,50)
+const runningAnimation: light.NeoPixelAnimation = new light.RunningLightsAnimation(0xFF, 0, 0, 50)
+
+let running = false
+function anim() {
+    if (running) return;
+    running = true;
+    control.runInBackground(() => {
+        while (running) {
+            lights1.startBrightnessTransition(0, 80, 500);
+            lights1.showAnimation(light.cometAnimation, 5000);
+        }
+    });
+    control.runInBackground(() => {
+        while (running) {
+            lights2.startBrightnessTransition(0, 80, 500);
+            lights2.showAnimation(light.cometAnimation, 5000);
+        }
+    });
+}
 
 touchLeft.onEvent(2, JDButtonEvent.Down, function () {
-    lights1.showAnimation(light.rainbowAnimation, 3000);
-    lights2.showAnimation(rainbowAnimation, 3000);
+    anim();
 })
 
 touchRight.onEvent(1, JDButtonEvent.Down, function () {
-    lights1.showAnimation(light.runningLightsAnimation, 3000);
-    lights2.showAnimation(runningAnimation, 3000);
+    anim();
 })
 
 touchLeft.onEvent(5, JDButtonEvent.Down, function () {
+    if (running) return;
     lights1.setAll(0xffff00);
     lights1.startBrightnessTransition(80, 0, 1000);
 })
 
 touchRight.onEvent(5, JDButtonEvent.Down, function () {
+    if (running) return;
     lights2.setAll(0x00ffff);
     lights2.startBrightnessTransition(80, 0, 1000);
 })
