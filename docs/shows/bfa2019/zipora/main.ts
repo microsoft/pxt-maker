7a476f493b0a6da098eb86818e7e62b969fccc99
Tom Ball <tball@microsoft.com>
2019-05-02 21:25:54 -0700

    victoria done

--- docs/shows/bfa2019/zipora/main.ts
+++ docs/shows/bfa2019/zipora/main.ts
@@ -1,20 +1,13 @@
 // gevanleen
 
-/*
 // hardware
 const lights1 = light.pixels;
 const lights2 = light.pixels2;
 
 // actions
 function pulse() {
-    lights1.startBrightnessTransition(0, 80, 1000, 2, true,
-        new light.EasingBrightnessTransition(easing.linear, easing.linear)
-    );
-    lights2.startBrightnessTransition(0, 80, 1000, 2, true,
-        new light.EasingBrightnessTransition(easing.linear, easing.linear)
-    );
-}
 
+}
 
 lights1.setLength(46);
 lights1.setBrightness(0);
@@ -22,10 +15,9 @@ lights1.setAll(0xffffff);
 lights2.setLength(46);
 lights2.setBrightness(0);
 lights2.setAll(0xffffff);
-*/
 
-const testLights = light.pixels
-testLights.setLength(20);
+//const testLights = light.pixels
+//testLights.setLength(20);
 const motionLeft = new jacdac.AccelerometerClient("step");
 const motionRight = jacdac.accelerometerClient;
 
@@ -33,9 +25,11 @@ motionLeft.requiredDeviceName = "L"
 motionRight.requiredDeviceName = "R"
 
 motionLeft.onCustomGesture(BeadGesture.Step, function () {
-    testLights.setAll(0xff0000)
+    //testLights.setAll(0xff0000)
+    lights1.startBrightnessTransition(80, 0, 1000);
 })
 
 motionRight.onCustomGesture(BeadGesture.Step, function () {
-    testLights.setAll(0x0000ff)
+    //testLights.setAll(0x0000ff)
+    lights2.startBrightnessTransition(80, 0, 1000);
 })
