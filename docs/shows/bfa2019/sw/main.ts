a622eae585380d2d78739e1020a5898c6881f353
Tom Ball <tball@microsoft.com>
2019-05-03 08:24:23 -0700

    simone done

--- docs/shows/bfa2019/sw/main.ts
+++ docs/shows/bfa2019/sw/main.ts
@@ -1,47 +1,39 @@
 // SW
 
-// hardware
-const tattoo = jacdac.touchButtonsClient;
+// hardware: too many neopixels, sticks in bootloader mode
 const lights = light.pixels;
+const lights2 = light.pixels2;
+//const lights3 = light.pixels7;
+
 lights.setBrightness(200);
-lights.setLength(50);
+lights.setLength(120);
+lights.setBuffered(true);
+lights2.setBrightness(200);
+lights2.setLength(120);
+lights2.setBuffered(true);
+// lights3.setBrightness(200);
+// lights3.setLength(120);
+// lights3.setBuffered(true);
 
-
-function interpolateColor(color1: number, color2: number, factor: number) {
-    //  factor = easing.inOutCubic(factor);
-      let r1 = color1 >> 16
-      let r2 = color2 >> 16
-      let g1 = (color1 & 0xff00) >> 8
-      let g2 = (color2 & 0xff00) >> 8
-      let b1 = color1 & 0xff
-      let b2 = color2 & 0xff
-      let r = (r1 + factor * (r2 - r1)) | 0x00
-      let g = (g1 + factor * (g2 - g1)) | 0x00
-      let b = (b1 + factor * (b2 - b1)) | 0x00
-      return (r << 16) | (g << 8) | b;
-  };
-  // My function to interpolate between two colors completely, returning an array
-  function interpolateColors(color1: number, color2: number, steps: number, i: number) {
-      let stepFactor = i / (steps - 1);
-      return interpolateColor(color1, color2, stepFactor);
-  }
-  
-  
+let last = lights.length()-1
   function pulse() {
-      let mid = lights.length()>>1
-      for (let i = 0; i < mid; i++) {
-          lights.setPixelColor(i, interpolateColors(0xff0000, 0x0000FF, mid, i))
+      lights.clear();
+      for (let i = 0; i < 30; i++) {
+          lights.setPixelColor(last,0xCC00AA); lights.move(LightMove.Shift,-1); lights.show();
+          lights2.setPixelColor(last,0xFFFF00); lights2.move(LightMove.Shift,-1); lights2.show();
+          // lights3.setPixelColor(last,0xFFFF00); lights3.move(LightMove.Shift,-1); lights3.show();
+          pause(1);
       }
-      for (let i = 0; i < mid; i++) {
-          lights.setPixelColor(i+mid, interpolateColors(0x0000FF, 0xFF0000, mid, i))
+      for (let i = 0; i < lights.length(); i++) {
+        lights.move(LightMove.Shift,-1); lights.show();
+        lights2.move(LightMove.Shift,-1); lights2.show();
+        // lights3.move(LightMove.Shift,-1); lights3.show();
+        pause(1);
       }
-      lights.show();
-      lights.startBrightnessTransition(96, 16, 800, 1, true,
-          new light.EasingBrightnessTransition(easing.inOutCubic));
-  }
+}
 
   
 // events
-tattoo.onEvent(2, JDButtonEvent.Down, function () {
+input.onGesture(Gesture.Shake, function () {
     pulse();
 })
