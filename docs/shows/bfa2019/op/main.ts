a83d026e2b7bd22bedd3ee247c1fe909cc0cb567
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-03 10:57:55 -0700

    opera code

--- docs/shows/bfa2019/op/main.ts
+++ docs/shows/bfa2019/op/main.ts
@@ -1,19 +1,29 @@
-// Q
 
 // hardware
 const lights = light.pixels;
-lights.setBrightness(255);
+lights.setAll(0xf0000);
 lights.setLength(288)
+lights.startBrightnessTransition(96, 0, 1000)
+const sparkle = new light.SparkleAnimation(0xff, 0, 0, 20);
+
 const lights2 = light.pixels2;
-lights2.setBrightness(25);
 lights2.setLength(120)
+lights2.setBrightness(25);
+lights2.setAll(0xf0000);
+lights2.startBrightnessTransition(96, 0, 1000)
+    
+const proxmity = jacdac.proximityClient;
 
-lights2.startBrightnessTransition(200, 0, 1000, -1, false,
-    new light.EasingBrightnessTransition(easing.outCubic)
-);
+let started = false;
+jacdac.proximityClient.onEvent(JDPromixityEvent.Close, function () {
+    if (started) return;
 
-let sparkle = new light.SparkleAnimation(0xff,0,0, 30);
-
-forever(function() {
-    lights.showAnimation(sparkle, 500);
-})
+    started = true;
+    lights2.startBrightnessTransition(200, 0, 1000, -1, false,
+        new light.EasingBrightnessTransition(easing.outCubic)
+    );
+    lights.setBrightness(255);
+    forever(function () {
+        lights.showAnimation(sparkle, 500);
+    })
+});
