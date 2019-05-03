4454e1b8e09e38cc0e8a5e81edb46a3a9014771b
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-03 15:34:19 -0700

    updated student

--- docs/shows/bfa2019/student/main.ts
+++ docs/shows/bfa2019/student/main.ts
@@ -2,10 +2,15 @@
 const lights1 = light.pixels;
 //const motion = jacdac.accelerometerClient;
 lights1.setLength(120); // 62
-lights1.setAll(0xFF0000);
+lights1.setAll(0x0000ff);
 lights1.setBuffered(true);
 
+let cols = [0x0000ff, 0xff0000, 0xffff00, 0x00ff00];
+let coli = 0;
+
 function pulse() {
+    lights1.setAll(cols[coli]);
+    coli = (coli + 1) % cols.length;
     lights1.startBrightnessTransition(96, 0, 800, 1, true,
         new light.EasingBrightnessTransition(easing.inOutCubic));
 }
