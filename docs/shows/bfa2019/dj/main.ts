a749f70fd9e6a22bc7a52027f13f1cd664c45079
Tom Ball <tball@microsoft.com>
2019-05-03 12:22:49 -0700

    dj working

--- docs/shows/bfa2019/dj/main.ts
+++ docs/shows/bfa2019/dj/main.ts
@@ -1,14 +1,17 @@
 // dj
 
 // hardware
-const color = jacdac.colorSensorClient;
-const lights = [light.pixels, light.pixels2, light.pixels3];
+const lights = [light.pixels, light.pixels2, light.pixels5, light.pixels6];
+const colors = [0xFF0000, 0xFFFFFF, 0x00FFFF, 0xFFFF00]
+for(let i=0; i<lights.length; i++) {
+    lights[i].setLength(120);
+    lights[i].setBrightness(0);
+    lights[i].setAll(colors[i]);
+}
 
-// actions
-color.onStateChanged(function () {
-    const hue = color.lightHue();
-    lights.forEach(l => l.setAll(light.hsv(hue, 0xff, 0xff)));
+forever(function () {
+    for(let i=0; i<lights.length; i++) {
+        lights[i].startBrightnessTransition(80, 0, 1000);
+        pause(1000)
+    }
 })
-
-// events
-color.setStreaming(true);
