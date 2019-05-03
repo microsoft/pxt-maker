9b0651037ec08473019f20348f40b93871455da6
Tom Ball <tball@microsoft.com>
2019-05-02 19:47:19 -0700

    victory for victoria

--- docs/shows/bfa2019/vc/main.ts
+++ docs/shows/bfa2019/vc/main.ts
@@ -1,17 +1,16 @@
 // Victoria
 
-// wants pink lights
-
 // hardware
 const lights = light.pixels;
 lights.setLength(100);
 lights.setBrightness(255);
-lights.setColor(0xff3399);
 const motion = jacdac.accelerometerClient;
+
+let pinkSparkleAnimation: light.NeoPixelAnimation = new light.SparkleAnimation(0xff, 0x33, 0x99, 50);
 
 // actions
 function pulse() {
-    lights.showAnimation(light.sparkleAnimation, 2000)
+    lights.showAnimation(pinkSparkleAnimation, 2000)
 }
 
 // events
