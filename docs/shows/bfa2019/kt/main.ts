0a8dc70f29e78e42ac4b33f93c12a204a8c0724d
Tom Ball <tball@microsoft.com>
2019-05-02 21:43:45 -0700

    KT program

--- docs/shows/bfa2019/kt/main.ts
+++ docs/shows/bfa2019/kt/main.ts
@@ -11,10 +11,10 @@ function pulse() {
 
 lights1.setLength(46);
 lights1.setBrightness(0);
-lights1.setAll(0xffffff);
+lights1.setAll(0x00ff00);
 lights2.setLength(46);
 lights2.setBrightness(0);
-lights2.setAll(0xffffff);
+lights2.setAll(0x0000ff);
 
 //const testLights = light.pixels
 //testLights.setLength(20);
--- docs/shows/bfa2019/motion/main.ts
+++ docs/shows/bfa2019/motion/main.ts
@@ -1,2 +1,2 @@
 // motion
-jacdac.instance().setDeviceName("R");
+jacdac.instance().setDeviceName("L");
