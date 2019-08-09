ebb9711645393d443ac812bf4447af7ed558d77f
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-09 18:03:17 +0500

    Add files via upload

---
+++ libs/xinabox-cc03/config.ts
@@ -0,0 +1,15 @@
+namespace config {
+
+    export const PIN_LED_RED = DAL_PA04;
+	export const PIN_LED_GREEN = DAL_PA08;
+	export const PIN_LED_BLUE = DAL_PA09;
+    export const PIN_RX = DAL.PA11;
+    export const PIN_TX = DAL.PA10;
+    export const PIN_MISO = DAL.PA12;
+    export const PIN_MOSI = DAL.PB10;
+    export const PIN_SCK = DAL.PB11;
+    export const PIN_SCL = DAL.PA23;
+    export const PIN_SDA = DAL.PA22;
+    export const PIN_RXLED = PIN_RED_LED;
+    export const PIN_TXLED = PIN_GREEN_LED; 
+}
