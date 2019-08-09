b64ebbc462bb34d9d4b2c9425246202c105b21ac
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-09 20:09:55 +0500

    Update config.ts

--- libs/xinabox-cc03/config.ts
+++ libs/xinabox-cc03/config.ts
@@ -1,8 +1,8 @@
 namespace config {
 
-    export const PIN_LED_RED = DAL.PA4;
-    export const PIN_LED_GREEN = DAL.PA8;
-    export const PIN_LED_BLUE = DAL.PA9;
+    export const PIN_LED_RED = DAL.PA04;
+    export const PIN_LED_GREEN = DAL.PA08;
+    export const PIN_LED_BLUE = DAL.PA09;
     export const PIN_RX = DAL.PA11;
     export const PIN_TX = DAL.PA10;
     export const PIN_MISO = DAL.PA12;
