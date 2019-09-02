08fefbc3ce574b5ae67ae9326e9312a90759219f
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-02 18:17:23 +0500

    Update config.ts

--- libs/xinabox-cc03/config.ts
+++ libs/xinabox-cc03/config.ts
@@ -1,8 +1,9 @@
 namespace config {
 
-    export const PIN_LED_R = DAL.PA04;
-    export const PIN_LED_G = DAL.PA08;
-    export const PIN_LED_B = DAL.PA09;
+    export const PIN_LED_R = DAL.PA06;
+    export const PIN_LED_G = DAL.PA05;
+    export const PIN_LED_B = DAL.PA07;
+	
     export const PIN_RX = DAL.PA11;
     export const PIN_TX = DAL.PA10;
     export const PIN_MISO = DAL.PA12;
@@ -10,6 +11,14 @@ namespace config {
     export const PIN_SCK = DAL.PB11;
     export const PIN_SCL = DAL.PA23;
     export const PIN_SDA = DAL.PA22;
-    export const PIN_RXLED = DAL.PA04;
-    export const PIN_TXLED = DAL.PA08; 
+    export const PIN_RXLED = DAL.PA05;
+    export const PIN_TXLED = DAL.PA06;
+
+    export const PIN_FLASH_MISO = DAL.PA14;
+    export const PIN_FLASH_MOSI = DAL.PA08;
+    export const PIN_FLASH_SCK = DAL.PA09;
+    export const PIN_FLASH_CS = DAL.PA13;
+
+    export const PIN_JACK_TX = PIN_TX;
+    export const PIN_JACK_COMMLED = PIN_TXLED;	
 }
