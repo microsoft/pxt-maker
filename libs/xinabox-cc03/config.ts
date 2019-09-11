1cbd8df7ed37abc174138e59e03b378abaaad51f
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-11 19:39:59 +0500

    Removed unnecessary pins

--- libs/xinabox-cc03/config.ts
+++ libs/xinabox-cc03/config.ts
@@ -11,14 +11,4 @@ namespace config {
     export const PIN_SCK = DAL.PB11;
     export const PIN_SCL = DAL.PA23;
     export const PIN_SDA = DAL.PA22;
-    export const PIN_RXLED = DAL.PA05;
-    export const PIN_TXLED = DAL.PA06;
-
-    export const PIN_FLASH_MISO = DAL.PA14;
-    export const PIN_FLASH_MOSI = DAL.PA08;
-    export const PIN_FLASH_SCK = DAL.PA09;
-    export const PIN_FLASH_CS = DAL.PA13;
-
-    export const PIN_JACK_TX = PIN_TX;
-    export const PIN_JACK_COMMLED = PIN_TXLED;	
 }
