159c7640f1d96e8c9eee02f5049ef5c38cba4951
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-13 11:14:29 +0500

    Update device.d.ts

--- libs/xinabox-cs11/device.d.ts
+++ libs/xinabox-cs11/device.d.ts
@@ -1,15 +1,15 @@
 declare namespace pins {
     //% fixedInstance shim=pxt::getPin(PIN_LED_B)
     const PIN_LED_BLUE: DigitalInOutPin;
-	//% fixedInstance shim=pxt::getPin(PIN_LED_G)
+    //% fixedInstance shim=pxt::getPin(PIN_LED_G)
     const PIN_LED_GREEN: DigitalInOutPin;
-	//% fixedInstance shim=pxt::getPin(PIN_LED_R)
+    //% fixedInstance shim=pxt::getPin(PIN_LED_R)
     const PIN_LED_RED: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_RX)
     const RX: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_TX)
     const TX: DigitalInOutPin;
-	//% fixedInstance shim=pxt::getPin(PIN_MISO)
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
     const MISO: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_MOSI)
     const MOSI: DigitalInOutPin;
