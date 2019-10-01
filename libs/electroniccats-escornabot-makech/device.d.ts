95985082edde8485e02f8adf6b96f66ff5f0a350
Sabas <s@theinventorhouse.org>
2019-10-01 17:57:12 -0500

    Add Support for Escornabot makech (#251)
    * Initial bast pro mini m0
    
    * SVG temporal
    
    * Init definition Escornabot Makech
    
    * Remove capacitive buttons
    
    * Temporal SVGs
    
    * Update description and datasheet link
    
    * update graphics
    
    * update pxtarget
    
    * define leds in board json
    
    * add leds 1,2,3,4
    
    * Revert "Initial bast pro mini m0"
    
    This reverts commit 3077c70587c4e97ae0b253264c36ed79f4a143fa.
    
    * Revert "SVG temporal"
    
    This reverts commit 0a6c363ed9df5971e0b4c73fa76d0d64239a5ec1.
    
    * update link github
    
    * update pins ESP32
    
    * optimize image

---
+++ libs/electroniccats-escornabot-makech/device.d.ts
@@ -0,0 +1,76 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_LED1)
+    const LED1: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED2)
+    const LED2: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED3)
+    const LED3: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED4)
+    const LED4: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_A0)
+    const A0: AnalogOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A1)
+    const A1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A2)
+    const A2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A3)
+    const A3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A4)
+    const A4: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_D0)
+    const D0: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D1)
+    const D1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D2)
+    const D2: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D3)
+    const D3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D4)
+    const D4: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D5)
+    const D5: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D6)
+    const D6: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D7)
+    const D7: DigitalInOutPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_D8)
+    const D8: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D9)
+    const D9: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D10)
+    const D10: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D11)
+    const D11: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D12)
+    const D12: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D13)
+    const D13: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_RX)
+    const RX: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_TX)
+    const TX: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
+    const MISO: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MOSI)
+    const MOSI: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCK)
+    const SCK: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCL)
+    const SCL: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SDA)
+    const SDA: DigitalInOutPin;
+}
+
+declare namespace input {
+
+}
+
+declare namespace input {
+    
+}
