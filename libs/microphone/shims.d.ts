5dfd43e724963ff9dc3c8c3a12ae82bfba0b1e3a
Peli <jhalleux@microsoft.com>
2020-01-06 13:55:42 -0800

    bump to pxt 5.31.8

--- libs/microphone/shims.d.ts
+++ libs/microphone/shims.d.ts
@@ -25,7 +25,7 @@ declare namespace input {
     //% help=input/set-loud-sound-threshold
     //% blockId=input_set_loud_sound_threshold block="set loud sound threshold %value"
     //% parts="microphone"
-    //% value.min=1 value.max=100
+    //% value.min=1 value.max=255
     //% group="More" weight=14 blockGap=8 shim=input::setLoudSoundThreshold
     function setLoudSoundThreshold(value: int32): void;
 }
