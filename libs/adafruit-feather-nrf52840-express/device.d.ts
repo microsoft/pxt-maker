b9e960d4ce574280471ba602e4ecbada337e6f1a
Michał Moskal <michal@moskal.me>
2020-01-31 23:01:27 -0800

    Add support for Feather 840 (#278)
    * Port n840 branch - files for feather nrf52840
    
    * Implement allocateTimer()
    
    * Configure pins correctly
    
    * Fixes for '832
    
    * Move core---nrf* libraries to common pkgs
    
    * Remove outdated settings
    
    * Update NRF codal version
    
    * Use game---hw
    
    * Add missing D2
    
    * Bump pxt-c-p

---
+++ libs/adafruit-feather-nrf52840-express/device.d.ts
@@ -0,0 +1,103 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_A0)
+    const A0: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A1)
+    const A1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A2)
+    const A2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A3)
+    const A3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A4)
+    const A4: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A5)
+    const A5: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_D2)
+    const D2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D5)
+    const D5: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D6)
+    const D6: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D9)
+    const D9: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D10)
+    const D10: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D11)
+    const D11: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D12)
+    const D12: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D13)
+    const D13: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
+    const LED: DigitalInOutPin; // red
+    //% fixedInstance shim=pxt::getPin(PIN_LED2)
+    const LED2: DigitalInOutPin; // blue
+
+    //% fixedInstance shim=pxt::getPin(PIN_RX)
+    const RX: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_TX)
+    const TX: DigitalInOutPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
+    const MISO: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MOSI)
+    const MOSI: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCK)
+    const SCK: DigitalInOutPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_SCL)
+    const SCL: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SDA)
+    const SDA: DigitalInOutPin;
+}
+
+declare namespace input {
+    /**
+     * Button connecting A0 to GND.
+     */
+    //% block="button A0" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A0,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA0: Button;
+
+    /**
+     * Button connecting A1 to GND.
+     */
+    //% block="button A1" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A1,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA1: Button;
+
+    /**
+     * Button connecting A2 to GND.
+     */
+    //% block="button A2" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A2,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA2: Button;
+
+    /**
+     * Button connecting A3 to GND.
+     */
+    //% block="button A3" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A3,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA3: Button;
+
+    /**
+     * Button connecting A4 to GND.
+     */
+    //% block="button A4" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A4,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA4: Button;
+
+    /**
+     * Button connecting A5 to GND.
+     */
+    //% block="button A5" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_A5,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const buttonA5: Button;
+}
