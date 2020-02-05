19c1b51b89b220a806fb4d939bfd99c2a325592a
Peli <jhalleux@microsoft.com>
2020-02-04 16:00:03 -0800

    fix nano build

--- libs/arduino-nano-33-ble-sense/config.ts
+++ libs/arduino-nano-33-ble-sense/config.ts
@@ -1,10 +1,10 @@
 namespace config {
     //https://content.arduino.cc/assets/Pinout-NANOsense_latest.pdf
-    export const PIN_D12 = DAL.P1_08;
+    export const PIN_D12 = DAL.P1_8;
     export const PIN_MISO = PIN_D12;
-    export const PIN_D11 = DAL.P1_01;
+    export const PIN_D11 = DAL.P1_1;
     export const PIN_MOSI = PIN_D11;
-    export const PIN_D10 = DAL.P1_02;
+    export const PIN_D10 = DAL.P1_2;
     export const PIN_D9 = DAL.P0_27;
     export const PIN_D8 = DAL.P0_21;
     export const PIN_D7 = DAL.P0_23;
@@ -14,17 +14,17 @@ namespace config {
     export const PIN_D3 = DAL.P1_12;
     export const PIN_D2 = DAL.P1_11;
     export const PIN_RX = DAL.P1_10;
-    export const PIN_TX = DAL.P1_03;
-    export const PIN_A7 = DAL.P0_03;
+    export const PIN_TX = DAL.P1_3;
+    export const PIN_A7 = DAL.P0_3;
     export const PIN_A6 = DAL.P0_28;
-    export const PIN_A5 = DAL.P0_02;
+    export const PIN_A5 = DAL.P0_2;
     export const PIN_SCL = PIN_A5;
     export const PIN_A4 = DAL.P0_31;
     export const PIN_SDA = PIN_A4;
     export const PIN_A3 = DAL.P0_29;
     export const PIN_A2 = DAL.P0_30;
-    export const PIN_A1 = DAL.P0_05;
-    export const PIN_A0 = DAL.P0_04;
+    export const PIN_A1 = DAL.P0_5;
+    export const PIN_A0 = DAL.P0_4;
     export const PIN_D13 = DAL.P0_13;
     export const PIN_SCK = PIN_D13;
     export const PIN_LED = PIN_D13;
