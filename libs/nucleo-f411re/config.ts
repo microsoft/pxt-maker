efa9b6c10b7beeeafee73e4920a60aebdc4db37d
Michał Moskal <michal@moskal.me>
2020-02-19 13:44:55 -0800

    Add support for F411 (#292)
    * Add support for F411
    
    * Bump pxt-c-p

---
+++ libs/nucleo-f411re/config.ts
@@ -0,0 +1,42 @@
+namespace config {
+    export const PIN_A0 = DAL.PA_0;
+    export const PIN_A1 = DAL.PA_1;
+    export const PIN_A2 = DAL.PA_2;
+    export const PIN_A3 = DAL.PA_3;
+    export const PIN_A4 = DAL.PA_4;
+    export const PIN_A5 = DAL.PA_5;
+    export const PIN_A6 = DAL.PA_6;
+    export const PIN_A7 = DAL.PA_7;
+    export const PIN_A8 = DAL.PA_8;
+    export const PIN_A9 = DAL.PA_9;
+    export const PIN_A10 = DAL.PA_10;
+    export const PIN_A11 = DAL.PA_11;
+    export const PIN_A12 = DAL.PA_12;
+    export const PIN_A15 = DAL.PA_15;
+
+    export const PIN_B0 = DAL.PB_0;
+    export const PIN_B1 = DAL.PB_1;
+    export const PIN_B2 = DAL.PB_2;
+    export const PIN_B3 = DAL.PB_3;
+    export const PIN_B4 = DAL.PB_4;
+    export const PIN_B5 = DAL.PB_5;
+    export const PIN_B6 = DAL.PB_6;
+    export const PIN_B7 = DAL.PB_7;
+    export const PIN_B8 = DAL.PB_8;
+    export const PIN_B9 = DAL.PB_9;
+    export const PIN_B10 = DAL.PB_10;
+    export const PIN_B11 = DAL.PB_11;
+    export const PIN_B12 = DAL.PB_12;
+    export const PIN_B13 = DAL.PB_13;
+    export const PIN_B14 = DAL.PB_14;
+    export const PIN_B15 = DAL.PB_15;
+
+    export const PIN_SCK = PIN_A5;
+    export const PIN_MISO = PIN_A6;
+    export const PIN_MOSI = PIN_A7;
+
+    export const PIN_JACK_TX = PIN_A2;
+
+    export const PIN_LED = PIN_B3;
+    //export const PIN_LED = DAL.PC_9;
+}
