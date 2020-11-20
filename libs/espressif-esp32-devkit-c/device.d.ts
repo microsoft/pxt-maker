d972b081567605b20b44091ca91e3dac5c418713
Michal Moskal <michal@moskal.me>
2020-11-20 14:16:07 +0100

    Add esp32-devkitc

---
+++ libs/espressif-esp32-devkit-c/device.d.ts
@@ -0,0 +1,77 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
+    const LED: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_P36)
+    const VP: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P39)
+    const VN: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P34)
+    const P34: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P35)
+    const P35: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P32)
+    const P32: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P33)
+    const P33: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P25)
+    const P25: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P26)
+    const P26: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P27)
+    const P27: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P14)
+    const P14: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P12)
+    const P12: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P13)
+    const P13: PwmPin;
+
+    // flash: 9, 10, 11, 6, 7, 8 pins not exposed
+
+    //% fixedInstance shim=pxt::getPin(PIN_P23)
+    const P23: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P22)
+    const P22: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P1)
+    const P1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P3)
+    const P3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P21)
+    const P21: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P19)
+    const P19: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P18)
+    const P18: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P5)
+    const P5: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P17)
+    const P17: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P16)
+    const P16: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P4)
+    const P4: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P0)
+    const P0: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P2)
+    const P2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_P15)
+    const P15: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_TX)
+    const TX: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_RX)
+    const RX: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
+    const MISO: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MOSI)
+    const MOSI: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCK)
+    const SCK: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCL)
+    const SCL: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SDA)
+    const SDA: DigitalInOutPin;
+}
+
