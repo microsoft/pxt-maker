92319c9044b03ba106bb85b017e319d144d6d3e6
James Devine <t-jamdev@microsoft.com>
2021-03-03 21:12:26 +0000

    update pin map for machachi

--- libs/machachi/device.d.ts
+++ libs/machachi/device.d.ts
@@ -1,10 +1,8 @@
 declare namespace pins {
     //% fixedInstance shim=pxt::getPin(PIN_DOTSTAR_DATA)
     const APA102_DATA: DigitalInOutPin;
-
     //% fixedInstance shim=pxt::getPin(PIN_DOTSTAR_CLOCK)
     const APA102_CLOCK: DigitalInOutPin;
-
     //% fixedInstance shim=pxt::getPin(PIN_D0)
     const BUTTON_0: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_D1)
@@ -23,13 +21,53 @@ declare namespace pins {
     const BUTTON_7: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_D8)
     const JD_01: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_BTN_SOFT_RESET)
+    const SOFT_RESET: DigitalInOutPin;
 }
 
 declare namespace input {
 
-    //% block="button 0" fixedInstance
+    //% block="button east" fixedInstance
     //% shim=pxt::getButtonByPin(PIN_D0,BUTTON_ACTIVE_LOW_PULL_UP)
     //% parts="buttons"
-    const button0: Button;
+    const EAST: Button;
+    //% block="button south east" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D1,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const SOUTH_EAST: Button;
+    //% block="button south" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D2,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const SOUTH: Button;
+
+    //% block="button south west" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D3,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const SOUTH_WEST: Button;
+
+    //% block="button west" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D4,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const WEST: Button;
+
+    //% block="button north west" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D5,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const NORTH_WEST: Button;
+
+    //% block="button north" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D6,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const NORTH: Button;
+
+    //% block="button north east" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_D7,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const NORTH_EAST: Button;
+
+    //% block="button soft reset" fixedInstance
+    //% shim=pxt::getButtonByPin(PIN_BTN_SOFT_RESET,BUTTON_ACTIVE_LOW_PULL_UP)
+    //% parts="buttons"
+    const SOFT_RESET: Button;
 }
 
