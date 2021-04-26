d046625fb512e38ff122ab3d0b0e4568bdbe7450
James Devine <t-jamdev@microsoft.com>
2021-04-26 15:19:31 +0100

    add makeable target

---
+++ libs/microsoft-makeable/device.d.ts
@@ -0,0 +1,8 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_LED_R)
+    const LED_R: DigitalInOutPin; // red
+    //% fixedInstance shim=pxt::getPin(PIN_LED_G)
+    const LED_G: DigitalInOutPin; // green
+    //% fixedInstance shim=pxt::getPin(PIN_LED_B)
+    const LED_B: DigitalInOutPin; // blue
+}
