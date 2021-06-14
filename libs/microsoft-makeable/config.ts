6df08ca5892ec717afa94cff8634541f8671109b
James Devine <t-jamdev@microsoft.com>
2021-06-14 11:59:07 +0100

    update pinout for makeable board

--- libs/microsoft-makeable/config.ts
+++ libs/microsoft-makeable/config.ts
@@ -1,8 +1,21 @@
 namespace config {
-    export const PIN_JACK_TX = DAL.P0_13
-    export const PIN_A0 = DAL.P0_2;
+    export const PIN_LED = DAL.P0_0
 
-    export const PIN_LED_R = DAL.P0_14;
-    export const PIN_LED_G = DAL.P0_15;
-    export const PIN_LED_B = DAL.P0_16;
+    export const PIN_JACK_TX = DAL.P0_7
+    export const PIN_JACK_COMMLED = PIN_LED; // LED
+
+    export const PIN_A0 = DAL.P0_0;
+
+    export const PIN_D1 = DAL.P0_1
+    export const PIN_D3 = DAL.P0_3
+    export const PIN_D4 = DAL.P0_4
+    export const PIN_D5 = DAL.P0_5
+    export const PIN_D6 = DAL.P0_6
+
+    export const PIN_ILIM_FAULT0 = DAL.P0_26;
+    export const PIN_ILIM_FAULT1 = DAL.P0_25;
+    export const PIN_ILIM_FAULT2 = DAL.P0_24;
+
+    export const PIN_NEOPIXEL = DAL.P0_14;
+    export const NUM_NEOPIXELS = 4;
 }
