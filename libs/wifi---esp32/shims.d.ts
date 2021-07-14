90aa6f09238e4056b522aad7e48fd0f80cfaac77
Michal Moskal <michal@moskal.me>
2021-07-14 11:32:47 -0700

    import settings and wifi libs for esp32

---
+++ libs/wifi---esp32/shims.d.ts
@@ -0,0 +1,17 @@
+// Auto-generated. Do not edit.
+declare namespace _wifi {
+
+    /** Get ID used in events. */
+    //% shim=_wifi::eventID
+    function eventID(): int32;
+
+    /** Start a WiFi network scan. */
+    //% shim=_wifi::scanStart
+    function scanStart(): void;
+
+    /** Get the results of the scan if any. */
+    //% shim=_wifi::scanResults
+    function scanResults(): Buffer;
+}
+
+// Auto-generated. Do not edit. Really.
