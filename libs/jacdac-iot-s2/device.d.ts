1b9c76c08442716fbd9481fdcd1f1ee697c759a4
Michal Moskal <michal@moskal.me>
2021-07-14 11:31:56 -0700

    add jacdac-iot board

---
+++ libs/jacdac-iot-s2/device.d.ts
@@ -0,0 +1,13 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
+    const LED: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED_R)
+    const LED_R: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED_G)
+    const LED_G: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_LED_B)
+    const LED_B: PwmOnlyPin;
+}
