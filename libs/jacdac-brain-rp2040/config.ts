bd025ec5dee706584695cce4daba3afce369092f
Michal Moskal <michal@moskal.me>
2021-09-28 17:30:36 -0700

    add a hack

--- libs/jacdac-brain-rp2040/config.ts
+++ libs/jacdac-brain-rp2040/config.ts
@@ -3,7 +3,8 @@ namespace config {
     export const PIN_LED_R = 11 | DAL.CFG_PIN_CONFIG_ACTIVE_LO;
     export const PIN_LED_G = 13 | DAL.CFG_PIN_CONFIG_ACTIVE_LO;
     export const PIN_LED_B = 15 | DAL.CFG_PIN_CONFIG_ACTIVE_LO;
-    export const PIN_LED = PIN_LED_G;
+    // The early setting of this seems to cause crashes - to be investigated
+    // export const PIN_LED = PIN_LED_B;
 
     export const PIN_P0 = 0
     export const PIN_P1 = 1
